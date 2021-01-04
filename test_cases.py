1adf814d56fac3f12438b4695e70274d56e211bd
mkmysk123456789 <mkmysk123456789@gmail.com>
2021-01-04 10:56:27 +0900

    test_cases.pyの修正

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,71 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
+        # Bは正しい値でAが異常値の場合
+        #無効同値
+
+        #数値でない
         def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+                self.assertEqual (-1, calc('hello',123))
 
+        #小数点を持つ数値
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+                self.assertEqual (-1, calc(-1.2345,123))
 
+        #999より大きい整数
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+                self.assertEqual (-1, calc(1234,123))
 
+        #1未満の整数
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (-1, calc(0,123))
 
+
+        #有効同値
+
+        #1から999の間でA>Bとなる整数
+        def test_sample5 (self):
+                self.assertEqual (645498, calc(987,654))
+
+        #1から999の間でA<Bとなる整数
+        def test_sample6 (self):
+                self.assertEqual (39483, calc(123,321))
+
+        #1から999の間でA=Bとなる整数
+        def test_sample7 (self):
+                self.assertEqual (308025, calc(555,555))
+
+
+        # Aは正しい値でBが異常値の場合
+
+        #無効同値
+
+        #数値でない
+        def test_sample8 (self):
+                self.assertEqual (-1, calc(123,'hello'))
+
+        #小数点を持つ数値
+        def test_sample9 (self):
+                self.assertEqual (-1, calc(123,-1.2345))
+
+        #999より大きい整数
+        def test_sample10 (self):
+                self.assertEqual (-1, calc(123,1234))
+
+        #1未満の整数
+        def test_sample11 (self):
+                self.assertEqual (-1, calc(123,0))
+
+        # #有効同値
+
+        # #1から999の間でA>Bとなる整数
+        # def test_sample12 (self):
+        #         self.assertEqual (-1, calc(987,654))
+
+        # #1から999の間でA<Bとなる整数
+        # def test_sample13 (self):
+        #         self.assertEqual (-1, calc(123,321))
+
+        # #1から999の間でA=Bとなる整数
+        # def test_sample14 (self):
+        #         self.assertEqual (-1, calc(555,555))
