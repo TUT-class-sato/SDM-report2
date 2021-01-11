2836e90bf10498b8c8369aa9fb55991f4468d467
Osakabe Kyosuke <osakabe.kyosuke.am@tut.jp>
2021-01-11 16:55:55 +0900

    関数calc()とテストケースの修正

--- calc_mul.py
+++ calc_mul.py
@@ -6,11 +6,14 @@ def calc(A,B):
         ai=str(A)
         bi=str(B)
         p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
+        if isinstance(A, int) and isinstance(B, int):
+                if p.match(ai) or p.match(bi):
+                        a=float(ai)
+                        b=float(bi)
+                        if 0<a and a<1000 and 0<b and b<1000:
+                                valid=True
+                        else:
+                                valid=False
                 else:
                         valid=False
         else:
--- test_cases.py
+++ test_cases.py
@@ -6,16 +6,29 @@ from calc_mul import calc
 # Run with testrunner so needs to be in file test_
 
 class TestCalc (unittest.TestCase):
-
-        def test_sample1 (self):
+        def test_sample1 (self):	# Normal value
                 self.assertEqual (21, calc(3,7))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+        def test_sample2 (self):	# A is out of range
+                self.assertEqual (-1, calc(0,10))
 
-        def test_sample3 (self):
+        def test_sample3 (self):	# B is out of range
+                self.assertEqual (-1, calc(10,1000))
+
+        def test_sample4 (self):	# A is greater than B
+                self.assertEqual (21, calc(7,3))
+
+        def test_sample5 (self):	# Input value is a character
                 self.assertEqual (-1, calc('a','b'))
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+        def test_sample6 (self):	# Input value is a letter number
+                self.assertEqual (-1, calc('1','2'))
 
+        def test_sample7 (self):	# Input value is a character string
+                self.assertEqual (-1, calc("abc","def"))
+
+        def test_sample8 (self):	# Input value is a string number
+                self.assertEqual (-1, calc('10','20'))
+
+        def test_sample9 (self):	# Input value is decimal
+                self.assertEqual (-1, calc(0.1,999))
