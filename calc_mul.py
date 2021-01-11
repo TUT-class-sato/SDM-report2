9b842e93bdbdcc8ad3763f8bc76088a7d4b39798
tut203309 <usui.masataka.dt@tut.jp>
2021-01-11 17:22:46 +0900

    calc_mul.py: 仕様に合うよう修正

--- calc_mul.py
+++ calc_mul.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-
-import re
-                
+import re     
 def calc(A,B):
+        if isinstance(A,int) !=True:
+            return -1
+        if isinstance(B,int) !=True:
+            return -1
         ai=str(A)
         bi=str(B)
         p = re.compile('\d+(\.\d+)?')
         if p.match(ai) or p.match(bi):
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
+                if 0<a and a<1000 and 0<b and b<1000:
                         valid=True
                 else:
                         valid=False
@@ -21,8 +23,7 @@ def calc(A,B):
                 return ans
         else:
                 return -1
-        
-                
+                        
 def main ():
 	matchstring = ''
 	while matchstring != 'end':
