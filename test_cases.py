e7984c1d87a7bdb469a04369a01c52d6f6c9688f
tut203309 <usui.masataka.dt@tut.jp>
2021-01-11 17:11:49 +0900

    test_cases.py: テストケース作成

--- test_cases.py
+++ test_cases.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# coding: utf-8
 
 import unittest
 from calc_mul import calc
@@ -7,15 +8,46 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+        #同値分割(整数)
+        #(1より小さい範囲外整数,1より小さい範囲外整数)
+        def test_sample01 (self):
+               self.assertEqual (-1, calc(-10,-10))
+        #(1より小さい範囲外整数,範囲内整数)
+        def test_sample02 (self):
+               self.assertEqual (-1, calc(0,150))
+        #(範囲内整数A,範囲内整数B) 正しA<B
+        def test_sample03 (self):
+               self.assertEqual (3*7, calc(3,7))
+        #(範囲内整数A,範囲内整数B) 正しA>B
+        def test_sample04 (self):
+               self.assertEqual (3*7, calc(7,3))
+        #(範囲内整数,999より大きい範囲外整数)
+        def test_sample05 (self):
+                self.assertEqual (-1, calc(1,1000))
+        #(999より大きい範囲外整数,999より大きい範囲外整数)
+        def test_sample06 (self):
+                self.assertEqual (-1, calc(1000,2000))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
 
-        def test_sample3 (self):
+        #例外判定(小数)
+        #(範囲内整数,範囲内小数)
+        def test_sample07 (self):
+               self.assertEqual (-1, calc(3,7.1))
+        #(範囲内小数,範囲内小数)
+        def test_sample08 (self):
+               self.assertEqual (-1, calc(3.1,7.1))
+        
+
+        #例外判定(文字列)
+        #(数字無し文字列,数字無し文字列)
+        def test_sample09 (self):
                 self.assertEqual (-1, calc('a','b'))
+        #(数字有り文字列,数字有り文字列)
+        def test_sample10 (self):
+                self.assertEqual (-1, calc('a1','999b'))
+        #(数字のみ文字列,数字のみ文字列)
+        def test_sample11 (self):
+                self.assertEqual (-1, calc('1','999'))
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+
 
