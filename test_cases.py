b0643a386f17d2b6aa093dc5e139304bf5e8d5fa
KOU256 <kou256.8bit+github@gmail.com>
2021-01-15 03:13:23 +0900

    :white_check_mark: 同値分割法に基づいてテストケースを作成
    整数入力,小数入力,下限を下回る入力,上限を上回る入力,数字以外の文字列等の入力の各パターンをテストするテストケースを作成した.

--- test_cases.py
+++ test_cases.py
@@ -6,16 +6,50 @@ from calc_mul import calc
 # Run with testrunner so needs to be in file test_
 
 class TestCalc (unittest.TestCase):
+    # 有効同値(A,Bに1~999の整数を入力)
+    def test_accept(self):
+        self.assertEqual(250000, calc(500, 500))
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+    # 無効同値(A,Bに1〜999の小数を入力)
+    def test_float(self):
+        self.assertEqual(-1, calc(1.1, 998.9))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+    # 無効同値(A,Bに1未満の整数,小数を入力)
+    def test_lower_bound1(self):
+        self.assertEqual(-1, calc(0, 999))
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+    def test_lower_bound2(self):
+        self.assertEqual(-1, calc(0.9, 999))
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+    # 無効同値(A,Bに999より大きい整数,小数を入力)
+    def test_upper_bound1(self):
+        self.assertEqual(-1, calc(1, 1000))
+
+    def test_upper_bound2(self):
+        self.assertEqual(-1, calc(1, 999.1))
+
+    # 無効同値(A,Bに文字列,Noneを入力)
+    def test_non_number1(self):
+        self.assertEqual(-1, calc('a', 999))
+
+    def test_non_number2(self):
+        self.assertEqual(-1, calc('a', 'b'))
+
+    def test_non_number3(self):
+        self.assertEqual(-1, calc('1', 999))
+
+    def test_non_number4(self):
+        self.assertEqual(-1, calc('1', '999'))
+
+    def test_non_number5(self):
+        self.assertEqual(-1, calc('1.1', 999))
+
+    def test_non_number6(self):
+        self.assertEqual(-1, calc('1.1', '998.9'))
+
+    def test_non_number7(self):
+        self.assertEqual(-1, calc(None, 999))
+
+    def test_non_number8(self):
+        self.assertEqual(-1, calc(None, None))
 
