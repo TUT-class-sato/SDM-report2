fd54e4a11ab1c1c8a1e1805b28a663787bb9fae8
tatsuto <tatsutomcat@outlook.jp>
2021-01-16 01:16:09 +0900

    calc

--- calc_mul.py
+++ calc_mul.py
@@ -9,7 +9,7 @@ def calc(A,B):
         if p.match(ai) or p.match(bi):
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
+                if 0<a and a<b and b<1000 and a.is_integer() and b.is_integer():
                         valid=True
                 else:
                         valid=False
--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,19 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
+         # 1~999
         def test_sample1 (self):
                 self.assertEqual (21, calc(3,7))
-
+        # 0以下
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
-
+                self.assertEqual (-1, calc(-5,150))
+        # 1000以上
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
-
+                self.assertEqual (-1, calc(2000,150))
+        # 数字以外
         def test_sample4 (self):
+                self.assertEqual (-1, calc('a','b'))
+        # 整数以外 によるエラー処理ができていない
+        def test_sample5 (self):
                 self.assertEqual (-1, calc(0.1,999))
 
