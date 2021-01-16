7090f7f19763d0477cca3198949797b0879ccc49
G4RDS <account@g4rds.jp>
2021-01-16 16:28:24 +0900

    Fix accepting string as a value

--- calc_mul.py
+++ calc_mul.py
@@ -6,7 +6,9 @@ def calc(A,B):
         ai=str(A)
         bi=str(B)
         p = re.compile('^\d+$')
-        if p.match(ai) and p.match(bi):
+        if not isinstance(A, int) or not isinstance(B, int):
+                valid=False
+        elif p.match(ai) and p.match(bi):
                 a=float(ai)
                 b=float(bi)
                 if 0<a and a<1000 and 0<b and b<1000:
