21308a19f1678e812a24e5c7d767d990f776b0be
G4RDS <account@g4rds.jp>
2021-01-16 16:54:10 +0900

    Update testcases

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,43 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
+        # It multiplies given two values
         def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
-
+                self.assertEqual (3*7, calc(3,7))
+        
+        # It multiplies if A is greater than B
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
-
+                self.assertEqual (7*3, calc(7,3))
+        
+        # It fails if A is decimal
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
-
+                self.assertEqual (-1, calc(1.5,2))
+        
+        # It fails if B is decimal
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (-1, calc(2,1.5))
+        
+        # It fails if A is lower than １
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(0,7))
+        
+        # It fails if A is greater than 999
+        def test_sample6 (self):
+                self.assertEqual (-1, calc(1000,7))
+        
+        # It fails if B is lower than 0
+        def test_sample7 (self):
+                self.assertEqual (-1, calc(3,0))
+        
+        # It fails if B is greater than 999
+        def test_sample8 (self):
+                self.assertEqual (-1, calc(3,1000))
+        
+        # It fails if A is not an integer
+        def test_sample9 (self):
+                self.assertEqual (-1, calc("3",7))
+        
+        # It fails if B is not an integer
+        def test_sample10 (self):
+                self.assertEqual (-1, calc(3,"7"))
 
