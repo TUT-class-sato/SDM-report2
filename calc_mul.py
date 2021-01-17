a87922386fa9c236e627240bbf38c4af32354cfc
chess0001 <66056590+chess0001@users.noreply.github.com>
2021-01-17 17:51:03 +0900

    Update calc_mul.py

--- calc_mul.py
+++ calc_mul.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 import re
-                
+
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
+        p = re.compile('^\d+$')
+        if p.match(ai) and p.match(bi):
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
+                if 0<a and a<1000 and 0<b and b<1000 and a.is_integer() and b.is_integer():
                         valid=True
                 else:
                         valid=False
@@ -22,13 +22,11 @@ def calc(A,B):
         else:
                 return -1
         
-                
 def main ():
 	matchstring = ''
 	while matchstring != 'end':
                 A = input ('input A: ')
                 B = input ('input B: ')
                 print ('input A * input B = ', calc(A,B))
-
 if __name__ == '__main__':
 	main()
