288a576713d9d2d322c2c775f86d29e1fd4c9732
fillu87gyc <s1500604@sendai.kosen-ac.jp>
2021-01-17 22:07:48 +0900

    :bug:

--- calc_mul.py
+++ calc_mul.py
@@ -1,34 +1,36 @@
 #!/usr/bin/python3
 
 import re
-                
-def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-	p = re.compile('\d+$')        
-	if p.match(ai) and p.match(bi):
-                a=int(ai)
-                b=int(bi)
-		if (0 < a and a < 1000) and (0 < b and b < 1000):
-	                valid=True
-                else:
-                        valid=False
+
+
+def calc(A, B):
+    ai = str(A)
+    bi = str(B)
+    p = re.compile('\d+$')
+    if p.match(ai) and p.match(bi):
+        a = int(ai)
+        b = int(bi)
+        if (0 < a and a < 1000) and (0 < b and b < 1000):
+            valid = True
         else:
-                valid=False
-                
-        if valid:
-                ans=a*b
-                return ans
-        else:
-                return -1
-        
-                
-def main ():
-	matchstring = ''
-	while matchstring != 'end':
-                A = input ('input A: ')
-                B = input ('input B: ')
-                print ('input A * input B = ', calc(A,B))
+            valid = False
+    else:
+        valid = False
+
+    if valid:
+        ans = a*b
+        return ans
+    else:
+        return -1
+
+
+def main():
+    matchstring = ''
+    while matchstring != 'end':
+        A = input('input A: ')
+        B = input('input B: ')
+        print('input A * input B = ', calc(A, B))
+
 
 if __name__ == '__main__':
-	main()
+    main()
