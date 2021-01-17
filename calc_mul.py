cc449e44ca7e9354fa3038963c5f034aa96d3c3e
203376 <203376@users.noreply.github.com>
2021-01-18 03:49:47 +0900

    edit two files: calc_mul.py test_cases.py

--- calc_mul.py
+++ calc_mul.py
@@ -5,8 +5,10 @@ import re
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
+        #p = re.compile('\d+(\.\d+)?')
+        p = re.compile('\d{1,3}')
+
+        if p.fullmatch(ai) or p.fullmatch(bi):
                 a=float(ai)
                 b=float(bi)
                 if 0<a and a<b and b<1000:
--- test_cases.py
+++ test_cases.py
@@ -9,6 +9,29 @@ class TestCalc (unittest.TestCase):
 
         def test_sample1 (self):
                 self.assertEqual (21, calc(3,7))
+        
+        def test_sample2 (self):
+                self.assertEqual (999, calc(1,999))
+
+        def test_sample3 (self):
+                self.assertEqual (-1, calc(0,100))
+
+        def test_sample4 (self):
+                self.assertEqual (-1, calc(50,1000))
+
+        def test_sample5 (self):
+                self.assertEqual (-1, calc('a','b'))
+
+        def test_sample6 (self):
+                self.assertEqual (-1, calc(0.1,999.5))
+        
+        def test_sample7 (self):
+                self.assertEqual (-1, calc(2.5,50.5))
+
+
+        '''
+        def test_sample1 (self):
+                self.assertEqual (21, calc(3,7))
 
         def test_sample2 (self):
                 self.assertEqual (-1, calc(0,150))
@@ -18,4 +41,5 @@ class TestCalc (unittest.TestCase):
 
         def test_sample4 (self):
                 self.assertEqual (-1, calc(0.1,999))
+        '''
 
