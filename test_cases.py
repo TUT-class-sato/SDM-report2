7a395665bda245ea42e5d9e098131e283eb9c47b
B203316-tut <77497857+tut203316@users.noreply.github.com>
2021-01-18 14:25:18 +0900

    バグ修正

--- test_cases.py
+++ test_cases.py
@@ -19,3 +19,42 @@ class TestCalc (unittest.TestCase):
         def test_sample4 (self):
                 self.assertEqual (-1, calc(0.1,999))
 
+        def test_sameValue (self):
+                self.assertEqual (998001, calc(999,999))
+
+        def test_AisOvertheRange (self):
+                self.assertEqual (-1, calc(1000,1))
+
+        def test_BisOvertheRange (self):
+                self.assertEqual (-1, calc(1,1000))
+
+        def test_BothareOvertheRange (self):
+                self.assertEqual (-1, calc(1000,1000))
+
+        def test_AisBelowTheRange (self):
+        		self.assertEqual (-1, calc(0, 1))
+
+        def test_BisBelowTheRange (self):
+        		self.assertEqual (-1, calc(1, 0))
+
+        def test_BothareBelowTheRange (self):
+        		self.assertEqual (-1, calc(0, 0))
+
+        def test_AisFraction (self):
+        		self.assertEqual (-1, calc(0.1, 1))
+
+        def test_BisFraction (self):
+        		self.assertEqual (-1, calc(1, 0.1))
+
+        def test_BothareFraction (self):
+        		self.assertEqual (-1, calc(0.1, 0.1))
+
+        def test_AisNotNumber (self):
+        		self.assertEqual (-1, calc('a', 1))
+
+        def test_BisNotNumber (self):
+        		self.assertEqual (-1, calc(1, 'b'))
+
+        def test_BothareNotNumber (self):
+        		self.assertEqual (-1, calc('a', 'b'))
+
