fa5753a570b6b942b6ca0d68696213e81a3e476f
Seto Masaki <m171831@xdev01.edu.tut.ac.jp>
2021-01-18 16:01:39 +0900

    2021/01/18

--- calc_mul.py
+++ calc_mul.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
 
 import re
-                
+
+def is_int(s):
+    try:
+        int(s)
+    except:
+        return False
+    return True
+
+def is_integer_num(n):
+    if isinstance(n, int):
+        return True
+    if isinstance(n, float):
+        return n.is_integer()
+    return False
+
 def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
+    
+        if is_int(A) and is_int(B):
+            a = float(A)
+            b = float(B)
         else:
-                valid=False
-                
-        if valid:
+            return -1
+        
+        if 0<a<100 and 0<b<1000:
+            if is_integer_num(a) and is_integer_num(b):
                 ans=a*b
                 return ans
+	    else:
+		return -1
         else:
-                return -1
-        
+            return -1    
                 
 def main ():
 	matchstring = ''
