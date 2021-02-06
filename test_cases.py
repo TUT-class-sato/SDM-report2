12840a14e141eb1c38fc11a2104c83516a624a06
mon2929 <miagale55x39@gmail.com>
2021-02-06 21:04:39 +0900

    [add] 同値分割法に基づいたテストケースを作成

--- test_cases.py
+++ test_cases.py
@@ -19,3 +19,24 @@ class TestCalc (unittest.TestCase):
         def test_sample4 (self):
                 self.assertEqual (-1, calc(0.1,999))
 
+        def test_int_1_a(self):
+                self.assertEqual(-1, calc(-2, 500))
+        def test_int_2_a(self):
+                self.assertEqual(200000, calc(400, 500))
+        def test_int_3_a(self):
+                self.assertEqual(-1, calc(1002, 500))
+        def test_decimal_a(self):
+                self.assertEqual(-1, calc(499.9, 500))
+        def test_str_a(self):
+                self.assertEqual(-1, calc('abc', 500))
+
+        def test_int_1_b(self):
+                self.assertEqual(-1, calc(500, -2))
+        def test_int_2_b(self):
+                self.assertEqual(250000, calc(500, 500))
+        def test_int_3_b(self):
+                self.assertEqual(-1, calc(500, 1002))
+        def test_decimal_b(self):
+                self.assertEqual(-1, calc(500, 499.9))
+        def test_str_b(self):
+                self.assertEqual(-1, calc(500, 'abc'))
