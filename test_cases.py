72928682f89a98d3f9f6e421814275f0a9ec6249
hosoyuu <hosono.yuki.ql@tut.jp>
2022-01-17 17:41:19 +0900

    calc() の修正
    ・calc()を仕様に合うように修正
    ・test_cases にコメントを追加

--- test_cases.py
+++ test_cases.py
@@ -19,18 +19,19 @@ class TestCalc (unittest.TestCase):
         def test_sample4 (self):
                 self.assertEqual (-1, calc(0.1,999))
 
-
+        # test_case_1 : 文字を含む場合
         def test_case_1_1 (self):
                 self.assertEqual (-1, calc('a', 256))
 
         def test_case_1_2 (self):
                 self.assertEqual (-1, calc(1, 'b'))
         
+        # test_case_2 : 負の数を含む場合
         def test_case_2_1 (self):
-                self.assertEqual (-1, calc(-100, -10))
+                self.assertEqual (-1, calc(-100, -10)) # a < b
         
         def test_case_2_2 (self):
-                self.assertEqual (-1, calc(-10, -50))
+                self.assertEqual (-1, calc(-10, -50)) # a > b
         
         def test_case_2_3 (self):
                 self.assertEqual (-1, calc(100, -10))
@@ -38,20 +39,22 @@ class TestCalc (unittest.TestCase):
         def test_case_2_4 (self):
                 self.assertEqual (-1, calc(-100, 10))
 
+        # test_case_3 : 小数を含む場合
         def test_case_3_1 (self):
                 self.assertEqual (-1, calc(1.1, 2.2))
 
         def test_case_3_2 (self):
                 self.assertEqual (-1, calc(1, 2.2))
 
+        # test_case_4 : 整数の大小関係が異なる場合
         def test_case_4_1 (self):
-                self.assertEqual (15, calc(5, 3))
+                self.assertEqual (15, calc(5, 3)) # a > b
 
         def test_case_4_2 (self):
-                self.assertEqual (2500, calc(50, 50))
-        
+                self.assertEqual (2500, calc(50, 50)) # a = b
+
         def test_case_5 (self):
-                self.assertEqual (-1, calc(7, 1000))
-        
+                self.assertEqual (-1, calc(7, 1000)) # 1000丁度のとき
+
         def test_case_6 (self):
-                self.assertEqual (999, calc(1, 999))
+                self.assertEqual (999, calc(1, 999)) # 入力範囲の最小・最大値
