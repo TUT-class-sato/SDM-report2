730ca461543ce7fe75a5a963f8ac502b0795128f
suzukiryuya <suzuryu.4017@gmail.com>
2022-01-22 12:04:25 +0900

    入力する値の条件を変更

--- calc_mul.py
+++ calc_mul.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import re
-                
+
 def calc(A,B):
         ai=str(A)
         bi=str(B)
@@ -9,20 +9,20 @@ def calc(A,B):
         if p.match(ai) or p.match(bi):
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
+                if 0 < a < 1000 and 0 < b < 1000:
                         valid=True
                 else:
                         valid=False
         else:
                 valid=False
-                
+
         if valid:
                 ans=a*b
                 return ans
         else:
                 return -1
-        
-                
+
+
 def main ():
 	matchstring = ''
 	while matchstring != 'end':
