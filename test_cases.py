f06c824ec2c9845ad81322600e46627db429fbb0
Daichi <ikakarasu714@gmail.com>
2022-01-24 15:50:57 +0900

    [update]Add Test Case

--- test_cases.py
+++ test_cases.py
@@ -5,17 +5,45 @@ from calc_mul import calc
 
 # Run with testrunner so needs to be in file test_
 
+
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+    def test_sample1(self):
+        self.assertEqual(21, calc(3, 7))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+    def test_sample2(self):
+        self.assertEqual(-1, calc(0, 150))
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+    def test_sample3(self):
+        self.assertEqual(-1, calc('a', 'b'))
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+    def test_sample4(self):
+        self.assertEqual(-1, calc(0.1, 999))
 
+    # Add Test case
+    def test_sample5(self):
+        self.assertEqual(-1, calc(-1, 999))
+
+    def test_sample6(self):
+        self.assertEqual(-1, calc(1, -999))
+
+    def test_sample7(self):
+        self.assertEqual(999, calc(999, 1))
+
+    def test_sample8(self):
+        self.assertEqual(-1, calc(-0.5, 999))
+
+    def test_sample9(self):
+        self.assertEqual(-1, calc(999, -0.5))
+
+    def test_sample10(self):
+        self.assertEqual(-1, calc(-1, -999))
+
+    def test_sample11(self):
+        self.assertEqual(-1, calc(0.5, 0.5))
+
+    def test_sample12(self):
+        self.assertEqual(-1, calc(5, 1000))
+
+    def test_sample13(self):
+        self.assertEqual(-1, calc(2000, 10))
