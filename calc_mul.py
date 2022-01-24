54a7c6190d121c237a1a7de0b7d3321d75e79a92
Daichi <ikakarasu714@gmail.com>
2022-01-24 15:50:09 +0900

    [fix]calc_mul

--- calc_mul.py
+++ calc_mul.py
@@ -1,34 +1,38 @@
 #!/usr/bin/python3
 
 import re
-                
-def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
+
+
+def calc(A, B):
+    ai = str(A)
+    bi = str(B)
+    # p = re.compile('\d+(\.\d+)?')
+    p = re.compile('\A[1-9]\d*')
+    if p.fullmatch(ai) and p.fullmatch(bi):
+        a = float(ai)
+        b = float(bi)
+        # if 0 < a and a < b and b < 1000:
+        if a < 1000 and b < 1000:
+            valid = True
         else:
-                valid=False
-                
-        if valid:
-                ans=a*b
-                return ans
-        else:
-                return -1
-        
-                
-def main ():
-	matchstring = ''
-	while matchstring != 'end':
-                A = input ('input A: ')
-                B = input ('input B: ')
-                print ('input A * input B = ', calc(A,B))
+            valid = False
+    else:
+        valid = False
+
+    if valid:
+        ans = a*b
+        return ans
+    else:
+        return -1
+
+
+def main():
+    matchstring = ''
+    while matchstring != 'end':
+        A = input('input A: ')
+        B = input('input B: ')
+        print('input A * input B = ', calc(A, B))
+
 
 if __name__ == '__main__':
-	main()
+    main()
