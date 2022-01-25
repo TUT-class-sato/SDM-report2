1fb145bff5bb30d35a2963c98414b963b702e04b
b213331 <k213331@edu.tut.ac.jp>
2022-01-25 11:26:43 +0000

    同値分割法に基づいて，テストケースを作成して実装した

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,46 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+        #無効同値
+        def test_disable_null (self):
+                self.assertEqual (-1, calc(None, 500))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+        def test_disable_char1 (self):
+                self.assertEqual (-1, calc('a', 500))
+        
+        def test_disable_char2 (self):
+                self.assertEqual (-1, calc("ZZZ", 500))
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+        def test_disable_num_of_char (self):
+                self.assertEqual (-1, calc('1', 500))
+        
+        def test_disable_float_min (self):
+                self.assertEqual (-1, calc(1.1, 500))
+        
+        def test_disable_float_max (self):
+                self.assertEqual (-1, calc(998.9, 500))
+        
+        def test_disable_int_min (self):
+                self.assertEqual (-1, calc(0, 500))
+        
+        def test_disable_int_max (self):
+                self.assertEqual (-1, calc(1000, 500))
+        
+        def test_disable_Negative (self):
+                self.assertEqual (-1, calc(-500, 500))
+                
+        def test_disable_Negative2 (self):
+                self.assertEqual (-1, calc(-1000, 500))
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+        #有効同値
+        def test_enable1 (self):
+                self.assertEqual (999, calc(1, 999))
 
+        def test_enable2 (self):
+                self.assertEqual (999, calc(1.0, 999.0))
+        
+        def test_enable3 (self):
+                self.assertEqual (999, calc('1', '999'))
+        
+        def test_enable4 (self):
+                self.assertEqual (999, calc("1.0", "999.0"))
