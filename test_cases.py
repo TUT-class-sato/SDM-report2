08fd33785b9244445cb4536a70bb5e24db80d87d
NOMURA Fuga <f213353@xdev06.edu.tut.ac.jp>
2022-01-26 11:58:41 +0900

    report2-(2)(3)

--- test_cases.py
+++ test_cases.py
@@ -8,14 +8,13 @@ from calc_mul import calc
 class TestCalc (unittest.TestCase):
 
         def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
-
+                self.assertEqual (-1, calc(0.5,0.7))
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
-
+                self.assertEqual (-1, calc(999.5,999.7))
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
-
+                self.assertEqual (1000, calc(100,10))
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (-1, calc(10.5,20.5))
+        def test_sample5 (self):
+                self.assertEqual (-1, calc('a',10))
 
