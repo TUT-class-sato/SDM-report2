a741892b546d95f38536946985db33d4d3dcf906
miyake <miyake235711@gmail.com>
2022-01-26 11:50:33 +0900

    (2) テストケースの作成とlogをresult2-1に保存

--- test_cases.py
+++ test_cases.py
@@ -8,14 +8,16 @@ from calc_mul import calc
 class TestCalc (unittest.TestCase):
 
         def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+                self.assertEqual (21, calc(7,3))
 
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+                self.assertEqual (-1, calc(0.1,150))
 
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+                self.assertEqual (-1, calc(1,999.9))
 
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (-1, calc("a",999))
 
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(1.1,999))
