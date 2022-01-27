1269cb4f4765d61448404e719d2ce3f3eda110dc
UCHIYAMA Ryusei <r213305@xdev06.edu.tut.ac.jp>
2022-01-27 17:03:34 +0900

    (2)done

--- test_cases.py
+++ test_cases.py
@@ -19,3 +19,20 @@ class TestCalc (unittest.TestCase):
         def test_sample4 (self):
                 self.assertEqual (-1, calc(0.1,999))
 
+	def test_sample5 (self):
+		self.assertEqual (-1, calc(1.1,2.2))
+
+	def test_sample6 (self)
+		self.assertEqual (-1, calc(150,1000))
+
+	def test_sample7 (self)
+		self.assertEqual (-1, calc(1,999.1))
+
+	def test_sample8 (self)
+		self.assertEqual (1, calc(1,1))
+
+	def test_sample9(self):
+                self.assertEqual(2,calc(2,1))
+
+        def test_sample10 (self):
+                self.assertEqual(-1,calc('a',1))
