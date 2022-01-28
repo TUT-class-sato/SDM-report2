b649f107acd2b7953611dce48c2b6bd19f63d73a
Fumiyoshi MATANO <matano.fumiyoshi.fx@tut.jp>
2022-01-29 00:53:34 +0900

    report2(2)

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,26 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+    def t_sample1 (self):
+        self.assertEqual (21, calc(3,7))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+    def test_sample2_0 (self):
+        self.assertEqual (-1, calc(0,1000))
+    def test_sample2_1 (self):
+        self.assertEqual (-1, calc(0,150))
+    def test_sample2_2 (self): 
+        self.assertEqual (-1, calc(150,1000))
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
-
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
-
+    def test_sample3_0 (self):
+        self.assertEqual (-1, calc('a','b'))
+    def test_sample3_1 (self):                            
+        self.assertEqual (-1, calc('a',100))
+    def test_sample3_2 (self):
+        self.assertEqual (-1, calc(900,'b'))
+         
+    def test_sample4_0 (self):
+        self.assertEqual (-1, calc(0.1,998.9))
+    def test_sample4_1 (self):
+        self.assertEqual (-1, calc(0.1,999))
+    def test_sample4_2 (self):
+        self.assertEqual (-1, calc(1,998.9))
