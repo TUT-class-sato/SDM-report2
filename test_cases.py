3acb87075c0d740dedcee62e2858cf95cfc05d37
Fumiyoshi MATANO <matano.fumiyoshi.fx@tut.jp>
2022-01-29 10:43:29 +0900

    add gitignore

--- test_cases.py
+++ test_cases.py
@@ -10,12 +10,24 @@ class TestCalc (unittest.TestCase):
         def test_sample1 (self):
                 self.assertEqual (21, calc(3,7))
 
-        def test_sample2 (self):
+        def test_sample2_0 (self):
+                self.assertEqual (-1, calc(0,1000))
+        def test_sample2_1 (self):
                 self.assertEqual (-1, calc(0,150))
+        def test_sample2_2 (self):
+                self.assertEqual (-1, calc(150,1000))
 
-        def test_sample3 (self):
+        def test_sample3_0 (self):
                 self.assertEqual (-1, calc('a','b'))
+        def test_sample3_1 (self):
+                self.assertEqual (-1, calc('a',100))
+        def test_sample3_2 (self):
+                self.assertEqual (-1, calc(100,'b'))
 
-        def test_sample4 (self):
+        def test_sample4_0 (self):
+                self.assertEqual (-1, calc(0.1,998.9))
+        def test_sample4_1 (self):
                 self.assertEqual (-1, calc(0.1,999))
+        def test_sample4_2 (self):
+                self.assertEqual (-1, calc(1,998.9))
 
