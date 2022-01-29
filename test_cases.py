bd613c38dcfb090bd0a8580bf3f5e60955701734
yukikan <nitic.mtbr@gmail.com>
2022-01-29 19:58:06 +0900

    :white_check_mark: 同値分割法に基づいてテストケースを追加

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,71 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+        # 有効同値 1から999の整数
+        def test_accept1(self):
+                self.assertEqual(250000,calc(500,500))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+        def test_accept2(self):
+                self.assertEqual(999,calc(1,999))
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+        def test_accept3(self):
+                self.assertEqual(999,calc(999,1))
+        
+        # 無効同値 1未満の整数
+        def test_lowerbound1(self):
+                self.assertEqual(-1,calc(0,1))
+        
+        def test_lowerbound2(self):
+                self.assertEqual(-1,calc(-1,1))
+        
+        # 無効同値 1未満の小数
+        def test_lowerbound3(self):
+                self.assertEqual(-1,calc(0.9,1))
+        
+        def test_lowerbound4(self):
+                self.assertEqual(-1,calc(-0.9,1))
+        
+        # 無効同値 999より大きい整数
+        def test_upperbound1(self):
+                self.assertEqual(-1,calc(1,1000))
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+        def test_upperbound2(self):
+                self.assertEqual(-1,calc(1,1001))
+        
+        # 無効同値 999より大きい小数
+        def test_upperbound3(self):
+                self.assertEqual(-1,calc(1,999.9))
+                
+        def test_upperbound4(self):
+                self.assertEqual(-1,calc(1,1000.1))
+        
+        # 無効同値 1から999までの小数
+        def test_decimal(self):
+                self.assertEqual(-1,calc(1.1,998.9))
 
+        # 無効同値 数値ではない入力
+        def test_non_numeric1(self):
+                self.assertEqual(-1,calc('str',1))
+
+        def test_non_numeric2(self):
+                self.assertEqual(-1,calc('str','c'))
+
+        def test_non_numeric3(self):
+                self.assertEqual(-1,calc('1',999))
+
+        def test_non_numeric4(self):
+                self.assertEqual(-1,calc('1','999'))
+
+        def test_non_numeric5(self):
+                self.assertEqual(-1,calc('1.1','998.9'))
+        
+        # 無効同値 値の非存在を表すNone(数値ではない入力)
+        def test_non_numeric6(self):
+                self.assertEqual(-1,calc(None,999))
+                
+        def test_non_numeric7(self):
+                self.assertEqual(-1,calc(1,None))
+
+        def test_non_numeric8(self):
+                self.assertEqual(-1,calc(None,None))
+        
