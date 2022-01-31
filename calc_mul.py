6023fdd7ffc870f46f8c8488988560b019c4f1b2
masterK-R <kawaguchi.ryoma.ex@tut.jp>
2022-02-01 01:14:50 +0900

    log massage

--- calc_mul.py
+++ calc_mul.py
@@ -9,7 +9,7 @@ def calc(A,B):
         if p.match(ai) or p.match(bi):
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
+                if 0<a and a.is_integer() and b.is_integer() and a<1000 and b<1000:
                         valid=True
                 else:
                         valid=False
