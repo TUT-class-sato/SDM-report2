bbf1bfa7c3bf0e05f7ceb57c882afe49bd81e9d6
Tatsuya <tmimura20@gmail.com>
2022-01-31 14:58:31 +0900

    (2):done

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,33 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
+        # 有効な入力
         def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+                self.assertEqual (10, calc(1,10))
 
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+                self.assertEqual (10, calc(10,1))
 
+        # 以下，無効な入力
+
+        # 1未満の数値を含むケース
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
-
+                self.assertEqual (-1, calc(1,-1))
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (-1, calc(-1,1))
 
+        # 数値以外の入力が与えられるケース
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(10, 1000))
+        def test_sample6 (self):
+                self.assertEqual (-1, calc(1000, 10))
+
+        # 数値以外の入力が与えられるケース
+        def test_sample7 (self):
+                self.assertEqual (-1, calc(1.5, 1))
+        def test_sample8 (self):
+                self.assertEqual (-1, calc(1, 1.5))
+
+        # 入力に文字が含まれるケース
+        def test_sample9 (self):
+                self.assertEqual (-1, calc('a','b'))
