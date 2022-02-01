d4bc2cc67d4333a7a57e1f5de08397b7e363f005
OKAGUCHI Tomoya <t213309@xdev05.edu.tut.ac.jp>
2022-02-02 04:53:48 +0900

    test_case

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,27 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+        def test_case1 (self):
+                self.assertEqual (999, calc(1, 999))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+        def test_case2 (self):
+                self.assertEqual (999, calc(999, 1))
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+        def test_case3 (self):
+                self.assertEqual (-1, calc(0, 999))
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+        def test_case4 (self):
+                self.assertEqual (-1, calc(1, 1000))
+        
+        def test_case5 (self):
+                self.assertEqual (-1, calc(1.0, 1))
+        
+        def test_case6 (self):
+                self.assertEqual (-1, calc(1, 999.0))
+        
+        def test_case7 (self):
+                self.assertEqual (-1, calc('1ab', 1))
+        
+        def test_case8 (self):
+                self.assertEqual (-1, calc(1, '9c8'))
 
