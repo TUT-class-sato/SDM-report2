46ce88425d109f2ac9d40b626be70bdbb455fbb3
tominetaiki <tomine.taiki.pa@tut.jp>
2022-02-01 15:06:33 +0900

    all test cases, new calc_mul

--- calc_mul.py
+++ calc_mul.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
 
 import re
-                
+import unicodedata
+
+def zenkaku(s):#文字列に全角数偽が含まれるか判定
+        for i in s:
+                D=unicodedata.east_asian_width(i)
+                if 'F'==D:
+                        return False
+        return True
+
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('(\d+)*')#<正規表現の変更
-        if p.fullmatch(ai) and p.fullmatch(bi):#<文字列全体のマッチング
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<1000 and 0<b and b<1000:#<入力の定義域の変更
-                        valid=True
+        if zenkaku(ai) and zenkaku(bi):
+                p = re.compile('(\d+)*')#<正規表現の変更
+                if p.fullmatch(ai) and p.fullmatch(bi):#<文字列全体のマッチング
+                        a=float(ai)
+                        b=float(bi)
+                        if 0<a and a<1000 and 0<b and b<1000:#<入力の定義域の変更
+                                valid=True
+                        else:
+                                valid=False
                 else:
                         valid=False
         else:
