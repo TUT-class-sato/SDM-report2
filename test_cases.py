b3c590e7423541f53443ceca26323c86eb91d21e
keigoj <hojojo2000@gmail.com>
2022-02-01 18:40:17 +0900

    add testcases

--- test_cases.py
+++ test_cases.py
@@ -8,14 +8,64 @@ from calc_mul import calc
 class TestCalc (unittest.TestCase):
 
         def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+                self.assertEqual (35, calc(5,7))
 
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+                self.assertEqual (35, calc(7,5))
 
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+                self.assertEqual (9, calc(3,3))
 
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (-1, calc(0,0))
 
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(-1,100))
+
+        def test_sample6 (self):
+                self.assertEqual (-1, calc(100,-1))
+
+        def test_sample7 (self):
+                self.assertEqual (-1, calc(1000,1500))
+
+        def test_sample8 (self):
+                self.assertEqual (-1, calc(1000,150))
+
+        def test_sample9 (self):
+                self.assertEqual (-1, calc(100,1500))
+
+        def test_sample10 (self):
+                self.assertEqual (-1, calc(0.3,0.5))
+
+        def test_sample11 (self):
+                self.assertEqual (-1, calc(0.3,5))
+
+        def test_sample12 (self):
+                self.assertEqual (-1, calc(3,1.5))
+
+        def test_sample13 (self):
+                self.assertEqual (-1, calc('a','b'))
+
+        def test_sample14 (self):
+                self.assertEqual (-1, calc('c',500))
+
+        def test_sample15 (self):
+                self.assertEqual (-1, calc(600,'d'))
+
+        def test_sample16 (self):
+                self.assertEqual (150, calc('10','15'))
+
+        def test_sample17 (self):
+                self.assertEqual (150, calc('10',15))
+
+        def test_sample18 (self):
+                self.assertEqual (150, calc(10,'15'))
+
+        def test_sample19 (self):
+                self.assertEqual (-1, calc(None,None))
+
+        def test_sample20 (self):
+                self.assertEqual (-1, calc(None,3))
+
+        def test_sample21 (self):
+                self.assertEqual (-1, calc(3,None))
