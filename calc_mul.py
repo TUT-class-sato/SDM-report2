e2eb941b3933b095a33341bd972ab220f82b7309
tut213375 <95679715+tut213375@users.noreply.github.com>
2022-02-02 04:22:45 +0900

    fixed causes for failed tests
    note that input sanitizing function 'normalize' is unnecessary because native 'float' parser is surprisingly robust

--- calc_mul.py
+++ calc_mul.py
@@ -1,34 +1,46 @@
 #!/usr/bin/python3
 
 import re
-                
-def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
-        else:
-                valid=False
-                
-        if valid:
-                ans=a*b
-                return ans
-        else:
-                return -1
-        
-                
-def main ():
-	matchstring = ''
-	while matchstring != 'end':
-                A = input ('input A: ')
-                B = input ('input B: ')
-                print ('input A * input B = ', calc(A,B))
+import unicodedata
+
+
+def calc(A, B):
+    ai = str(A)
+    bi = str(B)
+    floatable = re.compile('^\d+(\.\d+)?$')
+    # consider also "?a:" ASCII flag ('\d' match only ASCII,
+    # not any char from Unicode category 'Number digit' [Nd])
+    if floatable.match(ai) and floatable.match(bi):
+        a = float(ai)
+        b = float(bi)
+        in_range = (1 <= a <= 999) and (1 <= b <= 999)
+        integers = (a == float(int(a))) and (b == float(int(b)))
+        valid = in_range and integers
+    else:
+        valid = False
+    if valid:
+        ans = a * b
+        return ans
+    else:
+        return -1
+
+
+def normalize(str_with_unicode_digits):
+    digit = re.compile('\d')
+    chars = [str(int(unicodedata.numeric(ch))) if digit.match(ch)
+             else ch
+             for ch in str_with_unicode_digits]
+    str_with_ascii_digits = "".join(chars)
+    return str_with_ascii_digits
+
+
+def main():
+    matchstring = ''
+    while matchstring != 'end':
+        A = input('input A: ')
+        B = input('input B: ')
+        print('input A * input B = ', calc(A, B))
+
 
 if __name__ == '__main__':
-	main()
+    main()
