f87e96a4a18cd5a61c9613526c604e47c02e383a
yutachaan <yutachan010907@gmail.com>
2023-01-26 02:05:28 +0900

    format test_cases.py and testrunner.py

--- test_cases.py
+++ test_cases.py
@@ -5,45 +5,45 @@ from calc_mul import calc
 
 # Run with testrunner so needs to be in file test_
 
-class TestCalc (unittest.TestCase):
-        def test_sample01(self):
-                self.assertEqual(-1, calc(0, 7))
+class TestCalc(unittest.TestCase):
+    def test_sample01(self):
+        self.assertEqual(-1, calc(0, 7))
 
-        def test_sample02(self):
-                self.assertEqual(150, calc(1, 150))
+    def test_sample02(self):
+        self.assertEqual(150, calc(1, 150))
 
-        def test_sample03(self):
-                self.assertEqual(99900, calc(999, 100))
+    def test_sample03(self):
+        self.assertEqual(99900, calc(999, 100))
 
-        def test_sample04(self):
-                self.assertEqual(-1, calc(1000, 8))
+    def test_sample04(self):
+        self.assertEqual(-1, calc(1000, 8))
 
-        def test_sample05(self):
-                self.assertEqual(-1, calc(0.1, 1))
+    def test_sample05(self):
+        self.assertEqual(-1, calc(0.1, 1))
 
-        def test_sample06(self):
-                self.assertEqual(-1, calc("100", 3))
+    def test_sample06(self):
+        self.assertEqual(-1, calc("100", 3))
 
-        def test_sample07(self):
-                self.assertEqual(800, calc(100, 8))
+    def test_sample07(self):
+        self.assertEqual(800, calc(100, 8))
 
-        def test_sample08(self):
-                self.assertEqual(-1, calc(7, 0))
+    def test_sample08(self):
+        self.assertEqual(-1, calc(7, 0))
 
-        def test_sample09(self):
-                self.assertEqual(150, calc(150, 1))
+    def test_sample09(self):
+        self.assertEqual(150, calc(150, 1))
 
-        def test_sample10(self):
-                self.assertEqual(99900, calc(100, 999))
+    def test_sample10(self):
+        self.assertEqual(99900, calc(100, 999))
 
-        def test_sample11(self):
-                self.assertEqual(-1, calc(8, 1000))
+    def test_sample11(self):
+        self.assertEqual(-1, calc(8, 1000))
 
-        def test_sample12(self):
-                self.assertEqual(-1, calc(1, 0.1))
+    def test_sample12(self):
+        self.assertEqual(-1, calc(1, 0.1))
 
-        def test_sample13(self):
-                self.assertEqual(-1, calc(3, "100"))
+    def test_sample13(self):
+        self.assertEqual(-1, calc(3, "100"))
 
-        def test_sample14(self):
-                self.assertEqual(800, calc(8, 100))
+    def test_sample14(self):
+        self.assertEqual(800, calc(8, 100))
--- testrunner.py
+++ testrunner.py
@@ -5,19 +5,16 @@ import sys
 
 loader = unittest.TestLoader()
 
-
 #Find the test files in the current directory
-
 tests = loader.discover('.')
 
 #Specify the level of information provided by the test runner
-
 testRunner = unittest.runner.TextTestRunner(verbosity=2)
 
-results=testRunner.run(tests)
+results = testRunner.run(tests)
 
 #print( "results: %s" % results )
 #print( "results.wasSuccessful: %s" % results.wasSuccessful() )
-if results.wasSuccessful()==False:
+if results.wasSuccessful() == False:
     sys.exit(1)
 
