60aae5a161d1457192f4425cd242d01e574730e9
tainosukeeeee <sekikawa0916@gmail.com>
2023-01-27 23:33:15 +0900

    modified calc_mul

--- calc_mul.py
+++ calc_mul.py
@@ -3,25 +3,30 @@
 import re
                 
 def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
-        else:
-                valid=False
-                
-        if valid:
-                ans=a*b
-                return ans
-        else:
-                return -1
+        if isinstance(A, int) and isinstance(B, int):
+                if (1 <= A <= 999) and (1 <= B <= 999):
+                        return A * B
         
+        return -1
+
+        # ai=str(A)
+        # bi=str(B)
+        # p = re.compile('\d+(\.\d+)?')
+        # if p.match(ai) or p.match(bi):
+        #         a=float(ai)
+        #         b=float(bi)
+        #         if 0<a and a<b and b<1000:
+        #                 valid=True
+        #         else:
+        #                 valid=False
+        # else:
+        #         valid=False
+                
+        # if valid:
+        #         ans=a*b
+        #         return ans
+        # else:
+        #         return -1 
                 
 def main ():
 	matchstring = ''
