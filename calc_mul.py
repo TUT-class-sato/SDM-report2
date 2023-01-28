0905116fb276918eb7fcceb55618e0431da2c69b
NAKAJIMA Haru <h201303@xdev02.edu.tut.ac.jp>
2023-01-28 12:38:12 +0900

    modify calc_mul.py

--- calc_mul.py
+++ calc_mul.py
@@ -5,19 +5,12 @@ import re
 def calc(A,B):
 	at = type(A)
 	bt = type(B)
-	#ai=str(A)
-	#bi=str(B)
-	#p = re.compile('\d+(\.\d+)?')
-	#if p.match(ai) or p.match(bi):
 	if (at is int) and (bt is int):
-		#a=float(ai)
-		#b=float(bi)
 		if A > B:
 			tmp = A
 			A = B
 			B = tmp
 		if 0<A and A<=B and B<1000:
-		#if 0<a and a<b and b<1000:
 			valid=True
 		else:
 			valid=False
@@ -25,7 +18,6 @@ def calc(A,B):
 		valid=False
                 
 	if valid:
-		#ans=a*b
 		ans = A * B
 		return ans
 	else:
