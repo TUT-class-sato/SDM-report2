c02506d502ed995133ed892e355be7e0d91456d1
NAKAJIMA Haru <h201303@xdev02.edu.tut.ac.jp>
2023-01-28 12:37:13 +0900

    modify calc_mul.py

--- calc_mul.py
+++ calc_mul.py
@@ -3,32 +3,41 @@
 import re
                 
 def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
-        else:
-                valid=False
+	at = type(A)
+	bt = type(B)
+	#ai=str(A)
+	#bi=str(B)
+	#p = re.compile('\d+(\.\d+)?')
+	#if p.match(ai) or p.match(bi):
+	if (at is int) and (bt is int):
+		#a=float(ai)
+		#b=float(bi)
+		if A > B:
+			tmp = A
+			A = B
+			B = tmp
+		if 0<A and A<=B and B<1000:
+		#if 0<a and a<b and b<1000:
+			valid=True
+		else:
+			valid=False
+	else:
+		valid=False
                 
-        if valid:
-                ans=a*b
-                return ans
-        else:
-                return -1
+	if valid:
+		#ans=a*b
+		ans = A * B
+		return ans
+	else:
+		return -1
         
                 
 def main ():
 	matchstring = ''
 	while matchstring != 'end':
-                A = input ('input A: ')
-                B = input ('input B: ')
-                print ('input A * input B = ', calc(A,B))
+		A = input ('input A: ')
+		B = input ('input B: ')
+		print ('input A * input B = ', calc(A,B))
 
 if __name__ == '__main__':
 	main()
