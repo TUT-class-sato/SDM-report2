1289663d9f4fb429246dcc1bdc839cee1c2fae61
Kota Takagi <kou@kotas-MBA.local>
2023-01-31 14:32:50 +0900

    Revert "Add: test_calc_mul.py / Fix: calc_mul.py"
    This reverts commit 64ccb93e4aaa09b0c54536ea34bdfc41b95efc2b.

--- calc_mul.py
+++ calc_mul.py
@@ -3,46 +3,26 @@
 import re
                 
 def calc(A,B):
-        # ai=str(A)
-        # bi=str(B)
-        # p = re.compile('\d+(\.\d+)?')
-        # if p.match(ai) or p.match(bi):
-        #         a=float(ai)
-        #         b=float(bi)
-        #         if 0<a and a<b and b<1000:
-        #                 valid=True
-        #         else:
-        #                 valid=False
-        # else:
-        #         valid=False
-                
-        # if valid:
-        #         ans=a*b
-        #         return ans
-        # else:
-        #         return -1
-
-        # 正規表現用
-        ai = str(A)
-        bi = str(B)
-        p = re.compile('[0-9０-９]+')
-
-        valid = True
-        # 小数，数字以外の文字列でないか
-        if p.fullmatch(ai) and p.fullmatch(bi):
-                # 指定範囲内（1~999）でないか
-                a = int(ai)
-                b = int(bi)
-                if (not (a >= 1 and a <= 999)) or (not (b >= 1 and b <= 999)):
-                        valid = False
+        ai=str(A)
+        bi=str(B)
+        p = re.compile('\d+(\.\d+)?')
+        if p.match(ai) or p.match(bi):
+                a=float(ai)
+                b=float(bi)
+                if 0<a and a<b and b<1000:
+                        valid=True
+                else:
+                        valid=False
         else:
-                valid = False
-
+                valid=False
+                
         if valid:
-                return a * b
+                ans=a*b
+                return ans
         else:
                 return -1
-
+        
+                
 def main ():
 	matchstring = ''
 	while matchstring != 'end':
