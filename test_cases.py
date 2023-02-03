f00fe03f8be2e6defa21ab25e8896357e8472e95
Bilguun <59075359+Bilguun-dotcom@users.noreply.github.com>
2023-02-03 11:39:54 +0900

    Edited the test_cases

--- test_cases.py
+++ test_cases.py
@@ -8,26 +8,23 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-    def expected_value(self):
+    def test_expected_value(self):
         self.assertEqual(21, calc(3, 7))
 
-    def boundary_value1(self):
+    def test_boundary_value1(self):
         self.assertEqual(-1, calc(0, 1000))
 
-    def boundary_value2(self):
+    def test_boundary_value2(self):
         self.assertEqual(999, calc(1, 999))
 
-    def outside_value(self):
+    def test_outside_value(self):
         self.assertEqual(-1, calc(-1, 1100))
 
-    def notnum_value(self):
+    def test_notnum_value(self):
         self.assertEqual(-1, calc('a', 'b'))
 
-    def decimal_value(self):
+    def test_decimal_value(self):
         self.assertEqual(-1, calc(0.1, 999))
 
-    def null_value(self):
-        self.assertEqual(-1, calc(None, 15))
-
-    def zero_value(self):
+    def test_zero_value(self):
         self.assertEqual(-1, calc(0, 15))
