d72d1583a03e2cb297678d7c0544745f90182e36
Bilguun <59075359+Bilguun-dotcom@users.noreply.github.com>
2023-02-04 16:55:42 +0900

    Fixed calc_mul and test_cases

--- test_cases.py
+++ test_cases.py
@@ -15,16 +15,46 @@ class TestCalc (unittest.TestCase):
         self.assertEqual(-1, calc(0, 1000))
 
     def test_boundary_value2(self):
+        self.assertEqual(-1, calc(1000, 0))
+
+    def test_boundary_value3(self):
+        self.assertEqual(-1, calc(1000, 1000))
+
+    def test_boundary_value4(self):
         self.assertEqual(999, calc(1, 999))
+
+    def test_boundary_value5(self):
+        self.assertEqual(999, calc(999, 1))
+
+    def test_boundary_value6(self):
+        self.assertEqual(998001, calc(999, 999))
 
     def test_outside_value(self):
         self.assertEqual(-1, calc(-1, 1100))
 
-    def test_notnum_value(self):
+    def test_outside_value1(self):
+        self.assertEqual(-1, calc(-1, 900))
+
+    def test_outside_value2(self):
+        self.assertEqual(-1, calc(900, -1))
+
+    def test_notnum_value1(self):
         self.assertEqual(-1, calc('a', 'b'))
 
-    def test_decimal_value(self):
+    def test_notnum_value2(self):
+        self.assertEqual(-1, calc('a', 10))
+
+    def test_notnum_value3(self):
+        self.assertEqual(-1, calc(10, 'a'))
+
+    def test_decimal_value1(self):
         self.assertEqual(-1, calc(0.1, 999))
 
-    def test_zero_value(self):
+    def test_decimal_value2(self):
+        self.assertEqual(-1, calc(999, 1.1))
+
+    def test_zero_value1(self):
         self.assertEqual(-1, calc(0, 15))
+
+    def test_zero_value2(self):
+        self.assertEqual(-1, calc(15, 0))
