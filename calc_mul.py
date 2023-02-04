df296c2006a25031c4a6574db62f04af27e0e131
tut223381 <120451920+tut223381@users.noreply.github.com>
2023-02-04 16:06:27 +0900

    Add files via upload
    updated files

--- calc_mul.py
+++ calc_mul.py
@@ -1,34 +1,33 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3 
+import re 
 
-import re
-                
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
-        else:
-                valid=False
-                
-        if valid:
-                ans=a*b
-                return ans
-        else:
+        #not a digit or int
+        if (not re.match("^[0-9 -]+$", ai) or not re.match("^[0-9 -]+$", bi)):
                 return -1
-        
-                
+        else:
+                a=int(ai)
+                b=int(bi)
+                """
+                #not a int
+                if a%1.0!=0.0 or b%1.0!=0.0:
+                        return -1
+                """
+                #out of range
+                if a<1 or a>=1000 or b<1 or b>=1000:
+                        return -1
+                else:
+                        return a*b
+
+
 def main ():
-	matchstring = ''
-	while matchstring != 'end':
+        matchstring = ''
+        while matchstring != 'end':
                 A = input ('input A: ')
                 B = input ('input B: ')
                 print ('input A * input B = ', calc(A,B))
 
 if __name__ == '__main__':
-	main()
+        main()
--- test_cases.py
+++ test_cases.py
@@ -6,16 +6,54 @@ from calc_mul import calc
 # Run with testrunner so needs to be in file test_
 
 class TestCalc (unittest.TestCase):
-
+        #test with normal case
         def test_sample1 (self):
                 self.assertEqual (21, calc(3,7))
 
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+                self.assertEqual (225, calc(15,15))
 
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+                self.assertEqual (129402, calc(237,546))
 
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (322625, calc(725,445))
 
+        #test with edge and just outside
+        def test_sample5 (self):
+                self.assertEqual (999, calc(1,999))
+
+        def test_sample6 (self):
+                self.assertEqual (999, calc(999,1))
+
+        def test_sample7 (self):
+                self.assertEqual (-1, calc(0,999))
+
+        def test_sample8 (self):
+                self.assertEqual (-1, calc(1,1000))
+
+        #test with values other than int type
+        def test_sample9 (self):
+                self.assertEqual (-1, calc('a','b'))
+
+        def test_sample10 (self):
+                self.assertEqual (-1, calc(0.4,150))
+
+        def test_sample11 (self):
+                self.assertEqual (-1, calc(150,7.4))
+
+        def test_sample12 (self):
+                self.assertEqual (-1, calc(None,999))
+
+        #overflow and underflow
+        def test_sample13 (self):
+                self.assertEqual (-1, calc(-2147483648,2))
+
+        def test_sample14 (self):
+                self.assertEqual (-1, calc(2147483647,2))
+
+        def test_sample15 (self):
+                self.assertEqual (-1, calc(2147483648,1))
+
+        def test_sample16 (self):
+                self.assertEqual (-1, calc(-2147483649,1))
