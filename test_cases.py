736df4200756838feaaaf71230eb86be44a14535
konn41 <konndoshunshun@gmail.com>
2023-02-04 20:47:21 +0900

    add test test_cases

--- test_cases.py
+++ test_cases.py
@@ -6,8 +6,8 @@ from calc_mul import calc
 # Run with testrunner so needs to be in file test_
 
 class TestCalc (unittest.TestCase):
-
-        def test_sample1 (self):
+        #境界値分割法
+        """def test_sample1 (self):
                 self.assertEqual (21, calc(3,7))
 
         def test_sample2 (self):
@@ -18,4 +18,15 @@ class TestCalc (unittest.TestCase):
 
         def test_sample4 (self):
                 self.assertEqual (-1, calc(0.1,999))
+        """
+        def test_min(self):
+                self.assertEqual(1, calc(1,1))
+        def test_max(self):
+                self.assertEqual(998001, calc(999,999))
+        def test_minout(self):
+                self.assertEqual(-1, calc(0,0))
+        def test_maxout(self):
+                self.assertEqual(-1, calc(1000, 1000))
+        def test_string(self):
+                self.assertEqual(-1, calc("te", "st"))
 
