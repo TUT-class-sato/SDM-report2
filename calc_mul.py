2b800d5f0ba3311619c2399ac7080f98850a19b8
Turuu <dorjpurev.turmunkh.rg@tut.jp>
2023-02-05 02:33:39 +0900

    improve calc_mul.py where necessary

--- calc_mul.py
+++ calc_mul.py
@@ -7,11 +7,17 @@ def calc(A,B):
         bi=str(B)
         p = re.compile('\d+(\.\d+)?')
         if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
+                try:
+                        a=float(ai)
+                        b=float(bi)
+                        if 0<a and a<1000 and 0<b and b<1000:
+                                if a.is_integer() and b.is_integer():
+                                        valid=True
+                                else:
+                                        valid=False
+                        else:
+                                valid=False
+                except ValueError:
                         valid=False
         else:
                 valid=False
