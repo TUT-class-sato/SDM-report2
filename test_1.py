05e7054a2b75f78d5d4504ad31175a0266bd026a
masaa1000 <73923474+masaa1000@users.noreply.github.com>
2023-02-06 15:31:14 +0900

    Add files via upload

---
+++ test_1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import unittest
+from calc_mul import calc
+
+# Run with testrunner so needs to be in file test_
+
+class TestCalc (unittest.TestCase):
+
+        def test_sample1 (self):
+                self.assertEqual (21, calc(3,7))
+
+        def test_sample2 (self):
+                self.assertEqual (-1, calc(0,150))
+
+        def test_sample3 (self):
+                self.assertEqual (-1, calc('a','b'))
+
+        def test_sample4 (self):
+                self.assertEqual (-1, calc('a',999))
+        
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(0.1,'b'))
+        
+        def test_sample6 (self):
+                self.assertEqual (-1, calc(-1,999))
+
+        def test_sample7 (self):
+                self.assertEqual (-1, calc(0.1,1000))
+
+        def test_sample8 (self):
+                self.assertEqual (-1, calc(-1,1000))
