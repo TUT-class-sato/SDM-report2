20aa748b50d21fb958a7fae34bd9fffbc1a8c135
tut223337 <tagawa.kota.qr@tut.jp>
2023-02-06 17:10:06 +0900

    modify files

--- test_cases.py
+++ test_cases.py
@@ -8,14 +8,23 @@ from calc_mul import calc
 class TestCalc (unittest.TestCase):
 
         def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
-
+                self.assertEqual (20000, calc(100,200)) #有効同値
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
-
+                self.assertEqual (-1, calc(1.1,5)) #無効同値(小数)
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
-
+                self.assertEqual (-1, calc('a',1)) #無効同値(a文字列)
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (-1, calc(1,'b')) #無効同値(b文字列)
+
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(0,100)) #境界値分析
+        def test_sample6 (self):
+                self.assertEqual (100, calc(1,100))
+        def test_sample7 (self):
+                self.assertEqual (10000, calc(100,100))
+        def test_sample8 (self):
+                self.assertEqual (99900, calc(100,999))
+        def test_sample9 (self):
+                self.assertEqual (-1, calc(100,1000))
+
 
