97d2be19cad245a3ea589b66b989f349529ca6ed
Bob <124553979+223348@users.noreply.github.com>
2023-02-06 17:48:35 +0900

    Add files via upload

--- calc_mul.py
+++ calc_mul.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-
 import re
-                
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
+        p=re.compile('[+-]?\d+')
+        if p.fullmatch(ai) and p.fullmatch(bi):
+                #print(ai)
+                #print(bi)
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
+                if 0<a and a<1000 and 0<b and b<1000:
                         valid=True
                 else:
                         valid=False
         else:
                 valid=False
-                
         if valid:
                 ans=a*b
                 return ans
         else:
                 return -1
-        
-                
 def main ():
-	matchstring = ''
-	while matchstring != 'end':
+        matchstring = ''
+        while matchstring != 'end':
                 A = input ('input A: ')
                 B = input ('input B: ')
-                print ('input A * input B = ', calc(A,B))
-
+                print ('input A input B = ', calc(A,B))
 if __name__ == '__main__':
-	main()
+        main()
