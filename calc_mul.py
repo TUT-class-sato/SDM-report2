a9d96d3ac7bb87b31f933e3d194f8051a5a96069
Watanabe Satoshi <satoshinabe220@gmail.com>
2024-01-22 11:55:35 +0900

    インデントを修正

--- calc_mul.py
+++ calc_mul.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
 
 import re
-        
+                
 def calc(A,B):
-    ai=str(A)
-    bi=str(B)
-    p = re.compile(r'^\d+$')
-    if p.match(ai) and p.match(bi):
-        a=float(ai)
-        b=float(bi)
-        if 0< min(a,b) and max(a,b) <1000:
-            valid=True
+        ai=str(A)
+        bi=str(B)
+        p = re.compile(r'^\d+$')
+        if p.match(ai) and p.match(bi):
+                a=float(ai)
+                b=float(bi)
+                if 0< min(a,b) and max(a,b) <1000:
+                        valid=True
+                else:
+                        valid=False
         else:
-            valid=False
-    else:
-        valid=False
+                valid=False
+                
+        if valid:
+                ans=a*b
+                return ans
+        else:
+                return -1
         
-    if valid:
-        ans=a*b
-        return ans
-    else:
-        return -1
-    
-        
+                
 def main ():
-    matchstring = ''
-    while matchstring != 'end':
-        A = input ('input A: ')
-        B = input ('input B: ')
-        print ('input A * input B = ', calc(A,B))
+        matchstring = ''
+        while matchstring != 'end':
+                A = input ('input A: ')
+                B = input ('input B: ')
+                print ('input A * input B = ', calc(A,B))
 
 if __name__ == '__main__':
-    main()
+        main()
