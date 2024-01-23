ee14568f90743d45e0a6bf5051d8e8335ddd4085
ricbolz <hendricovbolang@gmail.com>
2024-01-23 18:56:16 +0900

    add test case and fix bug in code

--- test_cases.py
+++ test_cases.py
@@ -17,5 +17,17 @@ class TestCalc (unittest.TestCase):
                 self.assertEqual (-1, calc('a','b'))
 
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (-1, calc(0.1,999))  
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(0,0))
+        def test_sample6 (self):
+                self.assertEqual (-1, calc(1,999.1))
+        def test_sample7 (self):
+                self.assertEqual (1, calc(1,1))
+        def test_sample8 (self):
+                self.assertEqual(1000, calc(2,500))
+        def test_sample9 (self):
+                self.assertEqual(6, calc(1.5,4.0))
+        def test_sample10 (self):
+                self.assertEqual(2.88, calc(1.2,2.4))
 
