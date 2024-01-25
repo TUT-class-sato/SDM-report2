c7f13a2a902a890a8cddd2715327f716cac905ad
EssenApfel <takasagosou.private@gmail.com>
2024-01-25 13:07:18 +0900

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,28 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+        def test_a_zero (self):
+                self.assertEqual (-1, calc(0,5))
+        def test_b_zero (self):
+                self.assertEqual (-1, calc(5,0))
+        def test_a_one (self):
+                self.assertEqual (5, calc(1,5))
+        def test_b_one (self):
+                self.assertEqual (5, calc(5,1))
+        def test_a_thousand (self):
+                self.assertEqual (-1, calc(1000,5))
+        def test_b_thousand (self):
+                self.assertEqual (-1, calc(5,1000))
+        def test_a_under_thousand (self):
+                self.assertEqual (4995, calc(999,5))
+        def test_b_under_thousand (self):
+                self.assertEqual (4995, calc(5,999))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
-
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
-
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
-
+        def test_a_string (self):
+                self.assertEqual (-1, calc('a',5))
+        def test_b_string (self):
+                self.assertEqual (-1, calc(5,'b'))
+        def test_a_float (self):
+                self.assertEqual (-1, calc(0.1,5))
+        def test_b_float (self):
+                self.assertEqual (-1, calc(5,0.1))
