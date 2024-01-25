1eb51e5907a45b7179ac70d084e1e1a68903d032
mikutsuboi <umitsubo3@gmail.com>
2024-01-25 17:23:53 +0900

    Fixed calc_mul.py and test_cases.py

--- calc_mul.py
+++ calc_mul.py
@@ -5,11 +5,12 @@ import re
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
+        p = re.compile('^[1-9][0-9]*$') #(1)文字列が数字のみで構成されている場合のみ
+        if p.match(ai) and p.match(bi): #(2)aとb両方が条件を満たしているか確認する
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
+                #if 0<a and a<b and b<1000:
+                if 1<=a and a<=999 and 1<=b and b<=999: #(2)aとbの計算許可範囲の再指定
                         valid=True
                 else:
                         valid=False
