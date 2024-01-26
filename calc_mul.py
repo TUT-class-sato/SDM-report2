0db9aa43fe77ee08529c16e78394e047aec88766
tamakikozi <kumagai.yuya.ud@tut.jp>
2024-01-26 18:37:52 +0000

    Add:calc_mul.py / Fix:calc_mul.py

--- calc_mul.py
+++ calc_mul.py
@@ -3,25 +3,25 @@
 import re
                 
 def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
+        #正則表現用のstr変換
+        ai = str(A)
+        bi = str(B)
+        p = re.compile('[0-9０-９]+')
+
+        if p.fullmatch(ai) and p.fullmatch(bi):
+                a = int(ai)
+                b = int(bi)
+                if (0<a and a<1000) and (0<b and b<1000):
+                        valid = True
                 else:
-                        valid=False
+                        valid = False
         else:
-                valid=False
-                
+                valid = False
+        
         if valid:
-                ans=a*b
-                return ans
+                return a*b
         else:
                 return -1
-        
                 
 def main ():
 	matchstring = ''
