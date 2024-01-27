21274cadd4a0824acb8ee4f536c14713f38b9ac6
matsumiyakoshi <matsumiya.koshi.kb@tut.jp>
2024-01-27 14:53:14 +0900

    second

--- calc_mul.py
+++ calc_mul.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
 
 import re
-                
+
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
+        p = re.compile('^[1-9]\d{,2}$')
+        if p.match(ai) and p.match(bi):
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
+                valid=True
         else:
                 valid=False
-                
+
         if valid:
                 ans=a*b
                 return ans
         else:
                 return -1
-        
-                
+
+
 def main ():
-	matchstring = ''
-	while matchstring != 'end':
+    matchstring = ''
+    while matchstring != 'end':
                 A = input ('input A: ')
                 B = input ('input B: ')
                 print ('input A * input B = ', calc(A,B))
 
 if __name__ == '__main__':
-	main()
+    main()
+
