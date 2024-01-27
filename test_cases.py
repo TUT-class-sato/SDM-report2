ea48c1dfc3a7d51a7b0e5c370d7dd17ee134132c
Uchikai_Asahi <uchikai.asahi.fg@tut.jp>
2024-01-28 03:16:52 +0900

    calc_mul.py 	fix: 8行目 	<少数を受け付けないような正規表現に変更> 	fix: 9行目 	<orをandに変更> 	fix: 10~11行目 	<floatをint型に変更し整数のみを受け付けるように変更> 	fix: 12行目 	<ifの条件を1<=a,b<=999に変更> 	test_cases.py 	テストケースを追加

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,48 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
+        def test_sample1_1 (self):
                 self.assertEqual (21, calc(3,7))
 
-        def test_sample2 (self):
+        def test_sample1_2 (self):
+                self.assertEqual (21, calc(7,3))
+
+        def test_sample1_3 (self):
+                self.assertEqual (-1, calc(0,1000))
+
+        def test_sample1_4 (self):
+                self.assertEqual (9, calc(3,3))
+
+        def test_sample2_1 (self):
                 self.assertEqual (-1, calc(0,150))
 
-        def test_sample3 (self):
+        def test_sample2_2 (self):
+                self.assertEqual (-1, calc(150,1000))
+
+        def test_sample3_1 (self):
                 self.assertEqual (-1, calc('a','b'))
 
-        def test_sample4 (self):
+        def test_sample3_2 (self):
+                self.assertEqual (-1, calc(5,'b'))
+
+        def test_sample3_3 (self):
+                self.assertEqual (-1, calc('a',5))
+
+        def test_sample4_1 (self):
                 self.assertEqual (-1, calc(0.1,999))
+
+        def test_sample4_2 (self):
+                self.assertEqual (-1, calc(1,999.9))
+
+        def test_sample4_3 (self):
+                self.assertEqual (-1, calc(3.1,50.3))
+
+        def test_sample5_1 (self):
+                self.assertEqual (-1, calc(-1,1))
+
+        def test_sample5_2 (self):
+                self.assertEqual (-1, calc(1,-1))
+
+        def test_sample5_3 (self):
+                self.assertEqual (-1, calc(-10,-1))
 
