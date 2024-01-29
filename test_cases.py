a1b5b0d9455c9be40ba54db6fe2ebed079f7067d
unknown <hendricovbolang@gmail.com>
2024-01-29 15:06:26 +0900

    add new test case

--- test_cases.py
+++ test_cases.py
@@ -9,21 +9,29 @@ class TestCalc (unittest.TestCase):
 
         def test_sample1 (self):
                 self.assertEqual (21, calc(3,7))
-
         def test_sample2 (self):
                 self.assertEqual (-1, calc(0,150))
-
         def test_sample3 (self):
                 self.assertEqual (-1, calc('a','b'))
         def test_sample4 (self):
-                self.assertEqual(-1,calc('a',1))
+                self.assertEqual (-1, calc(0.1,999))
         def test_sample5 (self):
                 self.assertEqual (-1, calc(1,'b')) 
         def test_sample6 (self):
-                self.assertEqual (-1, calc(1,999.1))
+                self.assertEqual (-1, calc('a',1))
         def test_sample7 (self):
                 self.assertEqual(-1, calc(1.5,4.0))
         def test_sample8 (self):
                 self.assertEqual(1000, calc(2,500))
+        def test_sample9 (self):
+                self.assertEqual(-1, calc(None,1))
+        def test_sample10 (self):
+                self.assertEqual(-1, calc(1,None))
+        def test_sample11 (self):
+                self.assertEqual(-1, calc(None,None))
+        def test_sample12 (self):
+                self.assertEqual(1, calc(1,1))
+        def test_sample13 (self):
+                self.assertEqual(998001,calc(999,999))
         
 
