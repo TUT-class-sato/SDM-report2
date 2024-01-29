d3aa870c6eb7dd8ade56088518a62c2824037418
jiko-source <tobaru.koji.rc@tut.jp>
2024-01-29 14:41:13 +0900

    update test_cases,calc_mul

--- calc_mul.py
+++ calc_mul.py
@@ -3,13 +3,20 @@
 import re
                 
 def calc(A,B):
+        if "." in str(A) or "." in str(B):
+                return -1
+        if str(A).startswith('0') or str(B).startswith('0'):
+                return -1
+
         ai=str(A)
         bi=str(B)
         p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
+        if p.match(ai) and p.match(bi):
+                
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
+
+                if 1<=a  and b<=999 and (isinstance(a,(int,float))):
                         valid=True
                 else:
                         valid=False
@@ -24,11 +31,12 @@ def calc(A,B):
         
                 
 def main ():
-	matchstring = ''
-	while matchstring != 'end':
+        matchstring = ''
+        while matchstring != 'end':
                 A = input ('input A: ')
                 B = input ('input B: ')
                 print ('input A * input B = ', calc(A,B))
 
 if __name__ == '__main__':
-	main()
+        main()
+
--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,30 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
+        def test_sample11 (self):
                 self.assertEqual (21, calc(3,7))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+        def test_sample12 (self):
+                self.assertEqual (21, calc(7,3))
 
-        def test_sample3 (self):
+        def test_sample21 (self):
                 self.assertEqual (-1, calc('a','b'))
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+        def test_sample22 (self):
+                self.assertEqual (-1, calc(0,'s'))
+                
+        def test_sample23(self):
+                self.assertEqual(-1,calc(3.5,5.0))
+        
+        def test_sample31 (self):
+                self.assertEqual (-1, calc(999,1000))
+
+        def test_sample32 (self):
+                self.assertEqual (-1, calc(0.1,1))
+
+        def test_sample33 (self):
+                self.assertEqual (-1, calc('03','7'))
+
+        def test_sample34 (self):
+                self.assertEqual (21, calc(1+2,3+4))
 
