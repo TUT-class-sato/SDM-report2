cdb7364473cc83ecc912a8a7f7aa8fa5cc0e21cc
unknown <hendricovbolang@gmail.com>
2024-01-29 15:31:51 +0900

    edit test case and put the section

--- test_cases.py
+++ test_cases.py
@@ -6,32 +6,48 @@ from calc_mul import calc
 # Run with testrunner so needs to be in file test_
 
 class TestCalc (unittest.TestCase):
+        
+        # def test_sample1 (self):
+        #         self.assertEqual (21, calc(3,7))
+        # def test_sample2 (self):
+        #         self.assertEqual (-1, calc(0,150))
+        # def test_sample3 (self):
+        #         self.assertEqual (-1, calc('a','b'))
+        # def test_sample4 (self):
+        #         self.assertEqual (-1, calc(0.1,999))
 
-        def test_sample1 (self):
+        #Equivalence tests
+        def test_equivalencesample1 (self):
                 self.assertEqual (21, calc(3,7))
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
-        def test_sample5 (self):
-                self.assertEqual (-1, calc(1,'b')) 
-        def test_sample6 (self):
-                self.assertEqual (-1, calc('a',1))
-        def test_sample7 (self):
-                self.assertEqual(-1, calc(1.5,4.0))
-        def test_sample8 (self):
+        def test_equivalencesample2 (self):
                 self.assertEqual(1000, calc(2,500))
-        def test_sample9 (self):
+
+        #Other input tests
+        def test_othersample1 (self):
+                self.assertEqual (-1, calc('a','b'))
+        def test_othersample2 (self):
+                self.assertEqual (-1, calc(1,'b')) 
+        def test_othersample3 (self):
+                self.assertEqual (-1, calc('a',1))
+
+        #Decimal Fraction tests
+        def test_fractionsample1 (self):
+                self.assertEqual(-1, calc(1.5,4.0))
+        def test_fractionsample2 (self):
+                self.assertEqual (-1, calc(0.1,999))
+        
+        #Null tests
+        def test_nullsample1 (self):
                 self.assertEqual(-1, calc(None,1))
-        def test_sample10 (self):
+        def test_nullsample2 (self):
                 self.assertEqual(-1, calc(1,None))
-        def test_sample11 (self):
+        def test_nullsample3 (self):
                 self.assertEqual(-1, calc(None,None))
-        def test_sample12 (self):
+
+        #Boundary tests
+        def test_boundarysample1 (self):
                 self.assertEqual(1, calc(1,1))
-        def test_sample13 (self):
+        def test_boundary13 (self):
                 self.assertEqual(998001,calc(999,999))
         
 
