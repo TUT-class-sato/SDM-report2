849324180d0143a6209bc6a8a51c9dd6740a491f
SHIBUSAWA Miku <m211825@xdev04.edu.tut.ac.jp>
2024-01-30 23:25:13 +0900

    newcalc

--- test_cases.py
+++ test_cases.py
@@ -49,3 +49,62 @@ class TestCalc (unittest.TestCase):
         def test_sample14 (self):
                 self.assertEqual (-1, calc(500,1000))
 
+        def test_sample15 (self):
+                self.assertEqual (1, calc(1,1))
+
+        def test_sample16 (self):
+                self.assertEqual (998001, calc(999,999))
+
+        def test_sample17 (self):
+                self.assertEqual (999, calc(999,1))
+
+        def test_sample18 (self):
+                self.assertEqual (500, calc(500,1))
+
+        def test_sample19 (self):
+                self.assertEqual (-1, calc(1000,1))
+
+        def test_sample20 (self):
+                self.assertEqual (-1, calc(500,0))
+
+        def test_sample21 (self):
+                self.assertEqual (-1, calc(1000,0))
+
+        def test_sample22 (self):
+                self.assertEqual (499500, calc(999,500))
+
+        def test_sample23 (self):
+                self.assertEqual (-1, calc(999,0))
+
+        def test_sample24 (self):
+                self.assertEqual (-1, calc(1000,500))
+
+        def test_sample25 (self):
+                self.assertEqual (-1, calc(0,0))
+
+        def test_sample26 (self):
+                self.assertEqual (-1, calc(0,1))
+
+        def test_sample27 (self):
+                self.assertEqual (-1, calc(1,0))
+
+        def test_sample28 (self):
+                self.assertEqual (-1, calc(500,1000))
+
+        def test_sample29 (self):
+                self.assertEqual (-1, calc(999,1000))
+
+        def test_sample30 (self):
+                self.assertEqual (-1, calc(1000,999))
+
+        def test_sample31 (self):
+                self.assertEqual (-1, calc(1000,1000))
+
+        def test_sample32 (self):
+                self.assertEqual (-1, calc(50,'b'))
+
+        def test_sample33 (self):
+                self.assertEqual (-1, calc('a',50))
+
+        def test_sample34 (self):
+                self.assertEqual (-1, calc(50,0.1))
