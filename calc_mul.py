29ee53882de6eba266d64910e7745ee692a21c7e
KGON124 <158158780+KGON124@users.noreply.github.com>
2024-01-31 00:44:51 +0900

    ADD calc_mul.py_ver1.2

--- calc_mul.py
+++ calc_mul.py
@@ -6,7 +6,7 @@ def calc(A,B):
         ai=str(A)
         bi=str(B)
         p = re.compile('^\d+$') 
-        if p.match(ai) or p.match(bi):
+        if p.match(ai) and p.match(bi):
                 a=int(ai)
                 b=int(bi)
                 if 0<a<1000 and 0<b<1000:
