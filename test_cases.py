8c7bca5369ff41f25c9ded3e72450659cad0d565
KGON124 <158158780+KGON124@users.noreply.github.com>
2024-01-30 22:16:07 +0900

    ADD test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -6,16 +6,12 @@ from calc_mul import calc
 # Run with testrunner so needs to be in file test_
 
 class TestCalc (unittest.TestCase):
+    def test_valid_inputs(self):  #有効な入力
+        self.assertEqual(21, calc(3,7))  #正の整数
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
-
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
-
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
-
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
-
+    def test_invalid_inputs(self):
+        self.assertEqual(-1, calc(0,150))  # 入力Aが0
+        self.assertEqual(-1, calc(1000,150))  # 入力Aが1000
+        self.assertEqual(-1, calc('a','b'))  # 文字列
+        self.assertEqual(-1, calc(0.1,999))  # 入力A<Bではない
+        self.assertEqual(-1, calc(0.5,1))  # 正の小数
