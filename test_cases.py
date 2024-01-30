6eb66dbb78486e5f84fa9b0aa3be4d397e729126
rikuto290147 <87565090+rikuto290147@users.noreply.github.com>
2024-01-30 23:10:54 +0900

    create a test case

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,35 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+        def test_valid_integer1 (self):
+                self.assertEqual (1, calc(1,1))
+                
+        def test_valid_integer2 (self):
+                self.assertEqual (998001, calc(999,999))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+        def test_valid_integer3 (self):
+                self.assertEqual (200000, calc(400,500))
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+        def test_invalid_integer1 (self):
+                self.assertEqual (-1, calc(1,0))
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+        def test_invalid_integer2 (self):
+                self.assertEqual (-1, calc(999,1000))
+
+        def test_invalid_integer3 (self):
+                self.assertEqual (-1, calc(2000,4000))
+
+        def test_invalid_input1 (self):
+                self.assertEqual (-1, calc('a',1))
+
+        def test_invalid_input2 (self):
+                self.assertEqual (-1, calc('',1))
+
+        def test_invalid_input3 (self):
+                self.assertEqual (-1, calc(' ',1))
+
+        def test_invalid_input4 (self):
+                self.assertEqual (-1, calc(1.1,1))
+
+
 
