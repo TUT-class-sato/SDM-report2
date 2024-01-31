b85758db4ef41288e2e15f352d8450fd859e29b3
PERSONA-G14\zaidan <zaidanyahya28@gmail.com>
2024-01-31 15:50:58 +0900

    fix: file linter, add: gitignore

--- testrunner.py
+++ testrunner.py
@@ -6,18 +6,17 @@ import sys
 loader = unittest.TestLoader()
 
 
-#Find the test files in the current directory
+# Find the test files in the current directory
 
-tests = loader.discover('.')
+tests = loader.discover(".")
 
-#Specify the level of information provided by the test runner
+# Specify the level of information provided by the test runner
 
 testRunner = unittest.runner.TextTestRunner(verbosity=2)
 
-results=testRunner.run(tests)
+results = testRunner.run(tests)
 
-#print( "results: %s" % results )
-#print( "results.wasSuccessful: %s" % results.wasSuccessful() )
-if results.wasSuccessful()==False:
+# print( "results: %s" % results )
+# print( "results.wasSuccessful: %s" % results.wasSuccessful() )
+if results.wasSuccessful() is False:
     sys.exit(1)
-
