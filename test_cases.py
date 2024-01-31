29439f344f0c78c13d3220bd5a73a0a3e2b4c1cd
chobi-nyt <yao.chihiro.na@tut.jp>
2024-01-31 23:19:30 +0900

    report_all

--- test_cases.py
+++ test_cases.py
@@ -1,4 +1,4 @@
-99#!/usr/bin/python3
+#!/usr/bin/python3
 
 import unittest
 from calc_mul import calc
@@ -17,7 +17,7 @@ class TestCalc (unittest.TestCase):
                 self.assertEqual (-1, calc('a','b'))
 
         def test_sample4 (self):
-                self.assertEqual (-1, calc(1.5,999))
+                self.assertEqual (-1, calc(0.5,999))
 
 
 
