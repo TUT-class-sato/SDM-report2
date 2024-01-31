84f24b5bd6fd8807fbdbd9894ed675728196f91a
YAO Chihiro <c211851@xdev04.edu.tut.ac.jp>
2024-01-31 19:46:41 +0900

    report

--- calc_mul.py
+++ calc_mul.py
@@ -5,11 +5,17 @@ import re
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
+        #p = re.compile('\d+(\.\d+)?')
+        p = re.compile('^[1-9]\d*$')
+
+        #if p.match(ai) or p.match(bi):
+        if p.match(ai) and p.match(bi):
+                #a=float(ai)
+                #b=float(bi)
+                a=int(ai)
+                b=int(bi)
+                #if 0<a and a<b and b<1000:
+                if 1<=a<=999 and 1<=b<=999:
                         valid=True
                 else:
                         valid=False
