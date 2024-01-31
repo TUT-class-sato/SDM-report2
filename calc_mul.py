705aabf753399cfb00da295da1db8a9b797c13d2
mizukistut <seki.mizuki.ht@tut.jp>
2024-01-31 22:42:04 +0900

    update test_cases.py, calc.mul.py

--- calc_mul.py
+++ calc_mul.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 
 import re
+
+def is_integer(input_str):
+    pattern = r'^-?\d+$'
+    return bool(re.match(pattern, input_str))
                 
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
+        if is_integer(ai) and is_integer(bi):
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
+                if 0<a and a<1000 and 0<b and b<1000:
                         valid=True
                 else:
                         valid=False
--- test_cases.py
+++ test_cases.py
@@ -1,21 +1,76 @@
 #!/usr/bin/python3
 
 import unittest
+import sys
 from calc_mul import calc
 
 # Run with testrunner so needs to be in file test_
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+        def test_case1 (self):
+                self.assertEqual (10000, calc(100,100))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+        def test_case2 (self):
+                self.assertEqual (1, calc(1,1))
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+        def test_case3 (self):
+                self.assertEqual (999, calc(999,1))
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+        def test_case4 (self):
+                self.assertEqual (999, calc(1,999))
 
+        def test_case5 (self):
+                self.assertEqual (998001, calc(999,999))
+
+        def test_case6 (self):
+                self.assertEqual (-1, calc(1000,999))
+
+        def test_case7 (self):
+                self.assertEqual (-1, calc(999,1000))
+
+        def test_case8 (self):
+                self.assertEqual (-1, calc(1000,1000))
+
+        def test_case9 (self):
+                self.assertEqual (-1, calc(0,1))
+
+        def test_case10 (self):
+                self.assertEqual (-1, calc(1,0))
+
+        def test_case11 (self):
+                self.assertEqual (-1, calc(0,0))
+
+        def test_case12 (self):
+                self.assertEqual (-1, calc("abc",100))
+
+        def test_case13 (self):
+                self.assertEqual (-1, calc(100,"abc"))
+
+        def test_case14 (self):
+                self.assertEqual (-1, calc("abc","abc"))
+
+        def test_case15 (self):
+                self.assertEqual (-1, calc("",100))
+
+        def test_case16 (self):
+                self.assertEqual (-1, calc(100,""))
+
+        def test_case17 (self):
+                self.assertEqual (-1, calc("",""))
+
+        def test_case18 (self):
+                self.assertEqual (-1, calc(1.23,100))
+
+        def test_case19 (self):
+                self.assertEqual (-1, calc(100,1.23))
+
+        def test_case20 (self):
+                self.assertEqual (-1, calc(1.23,1.23))
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
