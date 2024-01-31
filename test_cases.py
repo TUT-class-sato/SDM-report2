872fe89c3912c558791d218332ea851d632a2ecf
PERSONA-G14\zaidan <zaidanyahya28@gmail.com>
2024-01-31 15:24:35 +0900

    update: add more test cases

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,31 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+        def test_valid_inputs_boundary_values(self):
+                self.assertEqual(1, calc(1, 1))  # min A, min B
+                self.assertEqual(999, calc(1, 999))  # min A, max B
+                self.assertEqual(250000, calc(500, 500))  # nominal
+                self.assertEqual(999, calc(999, 1))  # max A, min B
+                self.assertEqual(998001, calc(999, 999))  # max A, max B               
+        
+        def test_invalid_inputs_boundary_values(self):
+                self.assertEqual(-1, calc(0, 1))  # A below the lower boundary
+                self.assertEqual(-1, calc(1, 0))  # B below the lower boundary
+                self.assertEqual(-1, calc(1000, 1))  # A above the upper boundary
+                self.assertEqual(-1, calc(1, 1000))  # B above the upper boundary
+                self.assertEqual(-1, calc(0, 0))  # A, B below the lower boundary
+                self.assertEqual(-1, calc(1000, 1000))  # A, B above the upper boundary
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+        def test_invalid_inputs_string_values(self):
+                self.assertEqual(-1, calc(5.5, 500))  # A is floating number
+                self.assertEqual(-1, calc(500, 5.5))  # B is floating number
+                self.assertEqual(-1, calc(5.5, 5.5))  # A, B is floating number
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+        def test_invalid_inputs_string_values(self):
+                self.assertEqual(-1, calc("xyz", 500))  # A is a string 
+                self.assertEqual(-1, calc(500, "xyz"))  # B is a string
+                self.assertEqual(-1, calc("xyz", "xyz"))  # A, B is a string
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
-
+                self.assertEqual(-1, calc("z28", 500))  # A is a string and number
+                self.assertEqual(-1, calc(500, "z28"))  # B is a string and number
+                self.assertEqual(-1, calc("z28", "z28"))  # A, B is a string and number
