f88166f6bf4651410232f5052bde416686689309
syu-shu <syuriku0126@gmail.com>
2024-01-31 22:20:37 +0900

    [update] calc_mul.py

--- calc_mul.py
+++ calc_mul.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 
 import re
-                
+
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
+        p = re.compile('\d+')
+        if p.fullmatch(ai) is not None and p.fullmatch(bi) is not None:
+                a=int(ai)
+                b=int(bi)
+                if (0<a and a < 1000) and (0 < b and b < 1000):
                         valid=True
                 else:
                         valid=False
         else:
                 valid=False
-                
+
         if valid:
                 ans=a*b
                 return ans
         else:
                 return -1
-        
-                
+
+
 def main ():
 	matchstring = ''
 	while matchstring != 'end':
