5936cb6ed76390a5e9d71bb6096091eeeff46489
yosshi698 <hori.yoshiaki.lh@tut.jp>
2024-01-31 22:19:39 +0900

    fixed calc_mul.py changed test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,35 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
+        def test_case01 (self):
+                self.assertEqual (-1, calc(1000,10000))
+
+        def test_case02 (self):
+                self.assertEqual (-1, calc(-100,100))
+
+        def test_case03 (self):
+                self.assertEqual (-1, calc(100,-100))
+
+        def test_case04 (self):
                 self.assertEqual (21, calc(3,7))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+        def test_case05 (self):
+                self.assertEqual (-1, calc("10a",150))
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+        def test_case06 (self):
+                self.assertEqual (calc(7,3), calc(3,7))
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+        def test_case07 (self):
+                self.assertEqual (-1, calc(0,100))
 
+        def test_case08 (self):
+                self.assertEqual (100, calc(1,100))
+
+        def test_case09 (self):
+                self.assertEqual (99900, calc(100,999))
+
+        def test_case10 (self):
+                self.assertEqual (-1, calc(100,1000))
+
+        def test_case11 (self):
+                self.assertEqual (998001, calc(999,999))
