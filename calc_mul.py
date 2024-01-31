019a9304ab8942b95c8881b29ca346ccf5926f5e
kataoka1209 <18409@toba-cmt.ac.jp>
2024-01-31 22:22:10 +0900

    テストケースを変更、関数calc()を修正

--- calc_mul.py
+++ calc_mul.py
@@ -7,18 +7,28 @@ def calc(A,B):
         bi=str(B)
         p = re.compile('\d+(\.\d+)?')
         if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
+
+                try:#整数か小数かその他かを見分ける
+                        a = float(ai)
+                        b = float(bi)
+                        if a.is_integer() and b.is_integer():
+                                #整数
+                                if 0 < a and a < 1000 and 0 < b and b < 1000:
+                                        valid=True
+                                else:
+                                        valid = False
+                        else:
+                                #小数
+                                valid=False
+                except ValueError:
+                #その他
                         valid=False
         else:
                 valid=False
                 
         if valid:
                 ans=a*b
-                return ans
+                return int(ans)
         else:
                 return -1
         
--- test_cases.py
+++ test_cases.py
@@ -8,14 +8,32 @@ from calc_mul import calc
 class TestCalc (unittest.TestCase):
 
         def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+                self.assertEqual (7, calc(1,7))
 
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+                self.assertEqual (10, calc(10,1))
 
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+                self.assertEqual (1998, calc(999,2))
 
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (2997, calc(3,999))
+
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(10.1,10))
+        
+        def test_sample6 (self):
+                self.assertEqual (-1, calc(10,10.1))
+
+        def test_sample7 (self):
+                self.assertEqual (-1, calc(0,10))
+
+        def test_sample8 (self):
+                self.assertEqual (-1, calc(10,1000))
+
+        def test_sample9 (self):
+                self.assertEqual (-1, calc('abc',10))
+
+        def test_sample10 (self):
+                self.assertEqual (-1, calc(10,'xyz'))
 
