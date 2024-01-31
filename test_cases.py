1642fd498e9d0f95de269b72a460ef1ad40d24af
JunyaHondaTUT <honda.junya.qt@tut.jp>
2024-01-31 23:07:02 +0900

    update test_cases.py 01/31-23:07

--- test_cases.py
+++ test_cases.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
 
 import unittest
+
 from calc_mul import calc
 
 # Run with testrunner so needs to be in file test_
 
 class TestCalc (unittest.TestCase):
+        # representive value: {int_a: [3, 7], int_b: 0, int_c: 1000, float: 1.5, str: ["x", "y"]}
+        
+        def test_int_a (self):
+                self.assertEqual (9, calc(3, 3))
+                self.assertEqual (21, calc(3, 7))
+                self.assertEqual (21, calc(7, 3))
+                
+        def test_int_b (self):
+                self.assertEqual (-1, calc(0, 0))
+                self.assertEqual (-1, calc(0, 3))
+                self.assertEqual (-1, calc(3, 0))
+                
+        def test_int_c (self):
+                self.assertEqual (-1, calc(1000, 1000))
+                self.assertEqual (-1, calc(1000, 3))
+                self.assertEqual (-1, calc(3, 1000)) 
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
-
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
-
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
-
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
-
+        def test_float (self):
+                self.assertEqual (-1, calc(1.5, 1.5))
+                self.assertEqual (-1, calc(1.5, 3))
+                self.assertEqual (-1, calc(3, 1.5))
+        
+        def test_str (self):
+                self.assertEqual (-1, calc("x", "y"))
+                self.assertEqual (-1, calc("x", 3))
+                self.assertEqual (-1, calc(3, "x"))
+                
