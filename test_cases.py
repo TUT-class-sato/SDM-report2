9682f65832627439c91aeb2c72961cdbf72186cf
SuzuSys <sigmatics@outlook.jp>
2024-02-05 04:50:38 +0900

    Changed Test case

--- test_cases.py
+++ test_cases.py
@@ -8,14 +8,76 @@ from calc_mul import calc
 class TestCalc (unittest.TestCase):
 
         def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+                self.assertEqual (999, calc(1,999))
 
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+                self.assertEqual (999, calc(999,1))
 
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+                self.assertEqual (1, calc(1,1))
 
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (998001, calc(999,999))
+        
+        def test_sample5 (self):
+                self.assertEqual (999, calc(1, 999.0))
 
+        def test_sample6 (self):
+                self.assertEqual (999, calc(1.0, 999))
+        
+        def test_sample7 (self):
+                self.assertEqual (-1, calc(1, 1.4))
+
+        def test_sample8 (self):
+                self.assertEqual (-1, calc(1.4, 1))
+
+        def test_sample9 (self):
+                self.assertEqual (-1, calc(1, 1000))
+
+        def test_sample10 (self):
+                self.assertEqual (-1, calc(1000, 1))
+
+        def test_sample11 (self):
+                self.assertEqual (-1, calc(0, 999))
+
+        def test_sample12 (self):
+                self.assertEqual (-1, calc(999, 0))
+
+        def test_sample13 (self):
+                self.assertEqual (-1, calc(-1, 999))
+
+        def test_sample14 (self):
+                self.assertEqual (-1, calc(999, -1))
+
+        def test_sample15 (self):
+                self.assertEqual (-1, calc(1, 'a'))
+
+        def test_sample16 (self):
+                self.assertEqual (-1, calc('a', 1))
+
+        def test_sample17 (self):
+                self.assertEqual (-1, calc(1, '1'))
+
+        def test_sample18 (self):
+                self.assertEqual (-1, calc('1', 1))
+
+        def test_sample19 (self):
+                self.assertEqual (-1, calc(1, True))
+
+        def test_sample20 (self):
+                self.assertEqual (-1, calc(True, 1))
+
+        def test_sample21 (self):
+                self.assertEqual (-1, calc(1, [1]))
+
+        def test_sample22 (self):
+                self.assertEqual (-1, calc([1], 1))
+
+        def test_sample23 (self):
+                self.assertEqual (-1, calc(1, (1,)))
+
+        def test_sample24 (self):
+                self.assertEqual (-1, calc(1, {'1':1}))
+
+        def test_sample25 (self):
+                self.assertEqual (-1, calc({'1':1}, 1))
