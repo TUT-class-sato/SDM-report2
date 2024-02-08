c7346deb29d3295aab476a209c265b5a3f8714db
BAT BUYAN <131981249+Batbuyan21@users.noreply.github.com>
2024-02-08 18:30:37 +0900

    Update calc_mul.py

--- calc_mul.py
+++ calc_mul.py
@@ -1,34 +1,22 @@
-#!/usr/bin/python3
+def calc(A, B):
+    try:
+        a = int(A)
+        b = int(B)
+        if 1 <= a <= 999 and 1 <= b <= 999:
+            return a * b
+        else:
+            return -1
+    except ValueError:
+        return -1
 
-import re
-                
-def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
-        else:
-                valid=False
-                
-        if valid:
-                ans=a*b
-                return ans
-        else:
-                return -1
-        
-                
-def main ():
-	matchstring = ''
-	while matchstring != 'end':
-                A = input ('input A: ')
-                B = input ('input B: ')
-                print ('input A * input B = ', calc(A,B))
+
+def main():
+    matchstring = ''
+    while matchstring != 'end':
+        A = input('input A: ')
+        B = input('input B: ')
+        print('input A * input B = ', calc(A, B))
+
 
 if __name__ == '__main__':
-	main()
+    main()
