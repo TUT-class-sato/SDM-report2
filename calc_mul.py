8e4c84db5d85fd4b5813e1661876dd9de6ba82b0
Mitsuki4123 <mitsukikosen7@gmail.com>
2025-01-25 12:13:46 +0900

    関数の仕様変更

--- calc_mul.py
+++ calc_mul.py
@@ -3,32 +3,35 @@
 import re
                 
 def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
-        else:
-                valid=False
-                
-        if valid:
-                ans=a*b
-                return ans
-        else:
-                return -1
+	# 文字列変換
+	ai=str(A)
+	bi=str(B)
+	# 整数データか確認
+	p = re.compile('[0-9]+')
+	if p.fullmatch(ai) and p.fullmatch(bi):
+		a = int(ai)
+		b = int(bi)
+		# 整数範囲の確認
+		if (a > 0 and a < 1000) and (b > 0 and b < 1000):
+			valid = True
+		else:
+			valid = False
+	else:
+		valid = False
+
+	if valid:
+		ans = a * b
+		return ans
+	else:
+		return -1   
         
                 
 def main ():
 	matchstring = ''
 	while matchstring != 'end':
-                A = input ('input A: ')
-                B = input ('input B: ')
-                print ('input A * input B = ', calc(A,B))
+		A = input ('input A: ')
+		B = input ('input B: ')
+		print ('input A * input B = ', calc(A,B))
 
 if __name__ == '__main__':
 	main()
