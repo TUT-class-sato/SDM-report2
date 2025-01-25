7900dc57794a3c7b0534a7e8409bfa276ba1b5c9
unios103 <unios103@gmail.com>
2025-01-26 02:00:15 +0900

    課題3

--- calc_mul.py
+++ calc_mul.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
 
 import re
-                
+
+def is_in_range(a,b):
+        return 0<a and a<1000 and 0<b and b<1000
+
 def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
+        p = re.compile(r'^\d+$')
+        if p.match(str(A)) and p.match(str(B)):
+                a=int(A)
+                b=int(B)
+                if is_in_range(a,b):
                         valid=True
                 else:
                         valid=False
         else:
                 valid=False
-                
+
         if valid:
                 ans=a*b
                 return ans
         else:
                 return -1
-        
-                
+
+
 def main ():
-	matchstring = ''
-	while matchstring != 'end':
+        matchstring = ''
+        while matchstring != 'end':
                 A = input ('input A: ')
                 B = input ('input B: ')
                 print ('input A * input B = ', calc(A,B))
 
 if __name__ == '__main__':
-	main()
+        main()
