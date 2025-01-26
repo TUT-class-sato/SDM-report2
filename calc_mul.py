88e9101df331dbcf54f4379ffdadfe266ef77295
yuzurhyth <yuzurhyth@gmail.com>
2025-01-27 03:01:02 +0900

    fix calc_mul.py

--- calc_mul.py
+++ calc_mul.py
@@ -2,7 +2,7 @@
 
 import re
                 
-"""def calc(A, B):
+def calc(A, B):
         # AとBが整数であるかを確認
         if isinstance(A, int) and isinstance(B, int):
                 # 1 <= A, B <= 999 の範囲であるかを確認
@@ -11,28 +11,10 @@ import re
                 else:
                         return -1
         else:
-                return -1"""
-def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
-        else:
-                valid=False
-                
-        if valid:
-                ans=a*b
-                return ans
-        else:
                 return -1
         
-                
+        
+
 
         
                 
